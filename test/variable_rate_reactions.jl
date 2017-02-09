9c0b91d87ab1d4e9edb1bdf52cc99b515ebae9a1
ChrisRackauckas <me@chrisrackauckas.com>
2017-02-09 01:38:46 -0800

    add some decent tests

--- test/variable_rate_reactions.jl
+++ test/variable_rate_reactions.jl
@@ -14,6 +14,7 @@ srand(110)
 sol = solve(jump_prob,Tsit5())
 #using Plots; plotly(); plot(sol)
 @time sol = solve(jump_prob,Tsit5())
+@test 700 <= sol[end][3] <= 850
 
 #=
 # Takes Too Long For Travis
@@ -33,9 +34,10 @@ end
 prob = ODEProblem(f,[999.0,1.0,0.0,100.0],(0.0,250.0))
 jump_prob = GillespieProblem(prob,Direct(),r1,r2)
 
+srand(230)
 println("Add ODE to Gillespie Problem")
 sol = solve(jump_prob,Tsit5())
-
+@test 650 <= sol[end][3] <= 900
 
 r3 = VariableRateReaction(1e-2,[4],[(2,-1),(3,1)])
 prob = ODEProblem(f,[999.0,1.0,0.0,1.0],(0.0,250.0))
@@ -46,14 +48,16 @@ g = function (t,u,du)
   du[4] = 0.1u[4]
 end
 
+srand(330)
 println("Turn Gillespie Problem into SDE")
 prob = SDEProblem(f,g,[999.0,1.0,0.0,1.0],(0.0,250.0))
 jump_prob = GillespieProblem(prob,Direct(),r1,r2,r3)
 sol = solve(jump_prob,SRIW1())
-
+@test 650 <= sol[end][3] <= 1500
 
 println("Now mix constant rate reactions")
 r1 = Reaction(1e-4,(1,2),((1,-1),(2,1)))
 r2 = Reaction(0.01,[2],[(2,-1),(3,1)])
 jump_prob = GillespieProblem(prob,Direct(),r1,r2,r3)
 sol = solve(jump_prob,SRIW1())
+@test 650 <= sol[end][3] <= 1500
