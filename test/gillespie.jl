f6be8530cf828137f4164a275ebeb3831b81cd6e
Torkel Loman <Torkel.Loman@slpc385.slcu.private.cam.ac.uk>
2018-02-10 18:53:40 +0000

    Update Guillespie test file to work with new DSL.

--- test/gillespie.jl
+++ test/gillespie.jl
@@ -1,18 +1,23 @@
 using DiffEqBiological, DiffEqJump, DiffEqBase, OrdinaryDiffEq
 
-r1 = Reaction(1e-4,(1,2),((1,-1),(2,1)))
-r2 = Reaction(0.01,[2],[(2,-1),(3,1)])
+sir_model = @reaction_network rn begin
+    0.1/1000, s + i --> 2i
+    0.01, i --> r
+end
+
+sir_prob = DiscreteProblem([999,1,0],(0.0,250.0))
+sir_jump_prob = JumpProblem(sir_prob,Direct(),sir_model)
+sir_sol = solve(sir_jump_prob,Discrete())
 
 prob = DiscreteProblem([999,1,0],(0.0,250.0))
-jump_prob = GillespieProblem(prob,Direct(),r1,r2)
+jump_prob = JumpProblem(prob,Direct(),sir_model)
 
 srand(100)
 sol = solve(jump_prob,Discrete())
-#using Plots; plotly(); plot(sol)
 
 nums = Int[]
 @time for i in 1:1000
-  jump_prob = GillespieProblem(prob,Direct(),r1,r2)
+  jump_prob = JumpProblem(prob,Direct(),sir_model)
   sol = solve(jump_prob,Discrete())
   push!(nums,sol[end][3])
 end
@@ -23,7 +28,6 @@ f = function (du,u,p,t)
 end
 
 prob = ODEProblem(f,[999.0,1.0,0.0,100.0],(0.0,250.0))
-jump_prob = GillespieProblem(prob,Direct(),r1,r2)
+jump_prob = JumpProblem(prob,Direct(),sir_model)
 
 sol = solve(jump_prob,Tsit5())
-#plot(sol)
