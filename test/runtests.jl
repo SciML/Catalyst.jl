126180197d2ab3959fdb71f6165c33ec9bf46a72
Torkel Loman <Torkel.Loman@slpc385.slcu.private.cam.ac.uk>
2018-02-10 19:34:40 +0000

    Remove reference to old reaction_network DSL tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,6 @@ tic()
 @time @testset "Gillespie Tests" begin include("gillespie.jl") end
 @time @testset "Run Another Gillespie Tests" begin include("test_gillespie.jl") end
 @time @testset "Variable Rate Reaction Tests" begin include("variable_rate_reactions.jl") end
-#@time @testset "@reaction_network Tests" begin include("reaction_network.jl") end
 #@time @testset "Higher order reaction Tests" begin include("higher_order_reactions.jl") end
 
 @time @testset "Test the model creation macro" begin include("make_model.jl") end
