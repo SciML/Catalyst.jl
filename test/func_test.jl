77596cf51d4ff621677e746c36f26b08d9e6743b
Torkel Loman <Torkel.Loman@slpc385.slcu.private.cam.ac.uk>
2018-02-10 19:51:52 +0000

    Added tests to check if the noise scaling works correctly.

--- test/func_test.jl
+++ test/func_test.jl
@@ -26,3 +26,27 @@ for i = 1:100
     @test network1.jumps[2].rate(u,p,t) == network2.jumps[2].rate(u,p,t)
     @test network1.jumps[3].rate(u,p,t) == network2.jumps[3].rate(u,p,t)
 end
+
+model1 = @reaction_network rn begin
+    (5,5000), X ↔ 0
+end
+model2 = @reaction_network rn η begin
+    (5,5000), X ↔ 0
+end
+model3 = @reaction_network rn η begin
+    (1,5000), X ↔ 0
+end
+function tmp_std(sol)
+    vect = Vector{Float64}(length(sol.u))
+    for i = 1:length(sol.u)
+        vect[i] = sol.u[i][1]
+    end
+    return std(vect)
+end
+prob1 = SDEProblem(model1,[1000.],(0.,200.))
+prob2 = SDEProblem(model2,[1000.],(0.,200.), 0.1)
+prob3 = SDEProblem(model3,[1000.],(0.,200.),10)
+sol1 = solve(prob1, dt = 0.0001)
+sol2 = solve(prob2, dt = 0.0001)
+sol3 = solve(prob3, dt = 0.0001)
+@test tmp_std(sol2) < tmp_std(sol1) < tmp_std(sol3)
