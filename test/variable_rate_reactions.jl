f579e581b74fd5b40db4446a0adc0dcbaf1c7bce
Torkel Loman <Torkel.Loman@slpc385.slcu.private.cam.ac.uk>
2018-02-10 19:19:44 +0000

    old variable_rate_reaction test should now be used properly.

--- test/variable_rate_reactions.jl
+++ test/variable_rate_reactions.jl
@@ -1,48 +1,35 @@
 using DiffEqBiological, DiffEqJump, DiffEqBase, OrdinaryDiffEq, StochasticDiffEq
 using Base.Test
+using tmpMod
 
-r1 = VariableRateReaction(1e-4,(1,2),((1,-1),(2,1)))
-r2 = VariableRateReaction(0.01,[2],[(2,-1),(3,1)])
+sir_model = @reaction_network rn begin
+    0.1/1000, s + i --> 2i
+    0.01, i --> r
+end
 
 prob = DiscreteProblem([999.0,3.0,0],(0.0,250.0))
-jump_prob = GillespieProblem(prob,Direct(),r1,r2)
-
-jump_prob = JumpProblem(prob,Direct(),build_jumps_from_reaction(r1),build_jumps_from_reaction(r2))
-
-
-println("Solve the Gillespie Problem")
-srand(110)
-sol = solve(jump_prob,Tsit5())
-#using Plots; plotly(); plot(sol)
-@time sol = solve(jump_prob,Tsit5())
-@test 700 <= sol[end][3] <= 850
-
-#=
-# Takes Too Long For Travis
-nums = Int[]
-@time for i in 1:100
-  jump_prob = GillespieProblem(prob,Direct(),r1,r2)
-  sol = solve(jump_prob,Tsit5())
-  push!(nums,sol[end][3])
-end
-@test mean(nums) - 740 < 20
-=#
+jump_prob = JumpProblem(prob,Direct(),sir_model)
 
 f = function (du,u,p,t)
   du[4] = u[2]*u[3]/100000 - u[1]*u[2]/100000
 end
 
 prob = ODEProblem(f,[999.0,3.0,0.0,100.0],(0.0,250.0))
-jump_prob = GillespieProblem(prob,Direct(),r1,r2)
+jump_prob = JumpProblem(prob,Direct(),sir_model)
 
 srand(230)
 println("Add ODE to Gillespie Problem")
 sol = solve(jump_prob,Tsit5())
 @test 650 <= sol[end][3] <= 900
 
-r3 = VariableRateReaction(1e-2,[4],[(2,-1),(3,1)])
+sir_model = @reaction_network rn begin
+    0.1/1000, s + i --> 2i
+    0.01, i --> r
+    0.01d, d + i ⟾ d + r
+end
+
 prob = ODEProblem(f,[999.0,3.0,0.0,1.0],(0.0,250.0))
-jump_prob = GillespieProblem(prob,Direct(),r1,r2,r3)
+jump_prob = JumpProblem(prob,Direct(),sir_model)
 sol = solve(jump_prob,Tsit5())
 
 g = function (du,u,p,t)
@@ -52,14 +39,12 @@ end
 srand(331)
 println("Turn Gillespie Problem into SDE")
 prob = SDEProblem(f,g,[999.0,3.0,0.0,1.0],(0.0,250.0))
-jump_prob = GillespieProblem(prob,Direct(),r1,r2,r3)
+jump_prob = JumpProblem(prob,Direct(),sir_model)
 sol = solve(jump_prob,SRIW1())
 @test 650 <= sol[end][3] <= 1500
 
 println("Now mix constant rate reactions")
-r1 = Reaction(1e-4,(1,2),((1,-1),(2,1)))
-r2 = Reaction(0.01,[2],[(2,-1),(3,1)])
-jump_prob = GillespieProblem(prob,Direct(),r1,r2,r3)
+jump_prob = JumpProblem(prob,Direct(),sir_model)
 integrator = init(jump_prob,SRIW1())
 solve!(integrator)
 @test 650 <= sol[end][3] <= 1500
