21d2dfd24f0bebbdda81456032d679a877119176
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2018-02-13 01:37:30 -0800

    reaction network to DiffEqBase

--- src/DiffEqBiological.jl
+++ src/DiffEqBiological.jl
@@ -11,13 +11,10 @@ using DataStructures
 @reexport using DiffEqJump
 
 using Compat
-abstract type AbstractReactionNetwork end
 
 include("ReactionNetwork.jl")
 include("maketype.jl")
 include("problem.jl")
-
-export AbstractReactionNetwork
 
 export @reaction_network, @reaction_func
 export ODEProblem, SDEProblem, JumpProblem
