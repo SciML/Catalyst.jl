25c4727a09dc25831eb29ef36ac9b5de8789488b
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2018-03-14 12:13:55 -0700

    update Gillespie test

--- test/test_gillespie.jl
+++ test/test_gillespie.jl
@@ -7,14 +7,14 @@ end
 sir_prob = DiscreteProblem([999,1,0],(0.0,250.0))
 sir_jump_prob = JumpProblem(sir_prob,Direct(),sir_model)
 
-sir_sol = solve(sir_jump_prob,Discrete())
+sir_sol = solve(sir_jump_prob,FunctionMap())
 
 using Plots; plot(sir_sol)
 
 srand(1234)
 nums = Int[]
 @time for i in 1:100000
-  sir_sol = solve(sir_jump_prob,Discrete())
+  sir_sol = solve(sir_jump_prob,FunctionMap())
   push!(nums,sir_sol[end][3])
 end
 println("Reaction DSL: $(mean(nums))")
@@ -24,25 +24,25 @@ affect! = function (integrator)
   integrator.u[1] -= 1
   integrator.u[2] += 1
 end
-jump = ConstantRateJump(rate,affect!;save_positions=(false,true))
+jump = ConstantRateJump(rate,affect!)
 
 rate = (u,p,t) -> 0.01u[2]
 affect! = function (integrator)
   integrator.u[2] -= 1
   integrator.u[3] += 1
 end
-jump2 = ConstantRateJump(rate,affect!;save_positions=(false,true))
+jump2 = ConstantRateJump(rate,affect!)
 
 
 prob = DiscreteProblem([999.0,1.0,0.0],(0.0,250.0))
 jump_prob = JumpProblem(prob,Direct(),jump,jump2)
-sol = solve(jump_prob,Discrete(apply_map=false))
+sol = solve(jump_prob,FunctionMap())
 
 using Plots; plot(sol)
 
 nums = Int[]
 @time for i in 1:100000
-  sol = solve(jump_prob,Discrete(apply_map=false))
+  sol = solve(jump_prob,SSAStepper())
   push!(nums,sol[end][3])
 end
 println("Direct Jumps: $(mean(nums))")
@@ -66,7 +66,7 @@ srand(1234)
 
 nums = Int[]
 @time for i in 1:100000
-  result = ssa(x0,F,nu,parms,tf)
+  result = ssa(copy(x0),F,nu,parms,tf)
   data = ssa_data(result)
   push!(nums,data[:x3][end])
 end
