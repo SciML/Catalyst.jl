a480f2922af709ef125f739ef865399fc00f54a7
Gaussia <torkel.loman@gmail.com>
2018-03-23 14:48:48 +0000

    updated JumpProblem should work now.

--- src/problem.jl
+++ src/problem.jl
@@ -7,8 +7,12 @@ DiffEqBase.SDEProblem(rn::AbstractReactionNetwork, args...; kwargs...) =
     SDEProblem(rn.f,rn.g, args...;noise_rate_prototype=rn.p_matrix, kwargs...)
 
 ### JumpProblem ###
-DiffEqJump.JumpProblem(prob,aggregator::Direct,rn::AbstractReactionNetwork; kwargs...) =
+function DiffEqJump.JumpProblem(prob,aggregator::Direct,rn::AbstractReactionNetwork; kwargs...)
+    if typeof(prob)<:DiscreteProblem && any(issubtype.(typeof.(equi_model.jumps),VariableRateJump))
+        error("When using time dependant reaction rates a DiscreteProblem should not be used (try an ODEProblem). Also, use a continious solver.")
+    end
     JumpProblem(prob,aggregator::Direct,rn.jumps...;kwargs...)
+end
 
 ### SteadyStateProblem ###
 DiffEqBase.SteadyStateProblem(rn::AbstractReactionNetwork, args...; kwargs...) =
