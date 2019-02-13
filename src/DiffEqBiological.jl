420a6f0514ff5003db95ef05733bde6dd4c46a3a
Gaussia <torkel.loman@gmail.com>
2019-02-13 11:23:13 +0000

    - DynamicPolynomial is now reexported. - Inlucdes the equilibration utils file. - Exports some required equilibration functions.

--- src/DiffEqBiological.jl
+++ src/DiffEqBiological.jl
@@ -9,18 +9,19 @@ using SymEngine
 using MacroTools
 using DataStructures
 using Parameters
-using DynamicPolynomials
+@reexport using DynamicPolynomials
 @reexport using DiffEqBase, DiffEqJump
-
 using Compat
 
 include("reaction_network.jl")
 include("maketype.jl")
 include("massaction_jump_utils.jl")
 include("problem.jl")
+include("equilibrate_utils.jl")
 
 export @reaction_network, @reaction_func, @min_reaction_network
 export addodes!, addsdes!, addjumps!
 export ODEProblem, SDEProblem, JumpProblem, SteadyStateProblem
+export @fixed_concentration, add_fixed_concentration, fix_parameters, @make_hc_template, make_hc_template, steady_states
 
 end # module
