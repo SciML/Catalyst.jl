891c0298e9e98f3334a551b08b62999992ecc5f3
Gaussia <torkel.loman@gmail.com>
2019-02-13 16:31:57 +0000

    - Updated maketype with new fields.

--- src/maketype.jl
+++ src/maketype.jl
@@ -1,4 +1,4 @@
-function maketype(abstracttype, 
+function maketype(abstracttype,
                   name,
                   f,
                   f_func,
@@ -14,6 +14,8 @@ function maketype(abstracttype,
                   scale_noise;
                   params = Symbol[],
                   pfuncs=Vector{Expr}(undef,0),
+                  jac = nothing
+                  jac_prototype = nothing
                   symjac=Matrix{Expr}(undef,0,0),
                   reactions=Vector{ReactionStruct}(undef,0),
                   syms_to_ints = OrderedDict{Symbol,Int}(),
@@ -35,6 +37,8 @@ function maketype(abstracttype,
         p_matrix::Union{Array{Float64,2},Nothing}
         syms::Vector{Symbol}
         params::Vector{Symbol}
+        jac::Union{Function,Nothing}
+        jac_prototype::Nothing
         symjac::Union{Matrix{Expr},Nothing}
         reactions::Vector{ReactionStruct}
         syms_to_ints::OrderedDict{Symbol,Int}
@@ -58,11 +62,13 @@ function maketype(abstracttype,
                 $(Expr(:kw,:syms,syms)),
                 $(Expr(:kw,:params,params)),
                 $(Expr(:kw,:symjac,symjac)),
+                $(Expr(:kw,:jac,jac)),
+                $(Expr(:kw,:jac_prototype,jac_prototype)),
                 $(Expr(:kw,:reactions,reactions)),
                 $(Expr(:kw,:syms_to_ints, syms_to_ints)),
-                $(Expr(:kw,:params_to_ints, params_to_ints)), 
+                $(Expr(:kw,:params_to_ints, params_to_ints)),
                 $(Expr(:kw,:scale_noise, Meta.quot(scale_noise))),
-                $(Expr(:kw,:odefun, odefun)), 
+                $(Expr(:kw,:odefun, odefun)),
                 $(Expr(:kw,:sdefun, sdefun))) =
                 $(name)(
                         f,
@@ -77,6 +83,8 @@ function maketype(abstracttype,
                         p_matrix,
                         syms,
                         params,
+                        jac,
+                        jac_prototype,
                         symjac,
                         reactions,
                         syms_to_ints,
@@ -92,7 +100,7 @@ end
 
 # type function expressions
 function gentypefun_exprs(name; esc_exprs=true, gen_inplace=true, gen_outofplace=true, gen_constructor=true)
-    exprs = Vector{Expr}(undef,0)     
+    exprs = Vector{Expr}(undef,0)
 
     ## Overload the type so that it can act as a function.
     if gen_inplace
@@ -102,13 +110,13 @@ function gentypefun_exprs(name; esc_exprs=true, gen_inplace=true, gen_outofplace
 
     ## Add a method which allocates the `du` and returns it instead of being inplace
     if gen_outofplace
-        overloadex = :(((f::$name))(u,p,t::Number) = (du=similar(u); f(du,u,p,t); du)) 
+        overloadex = :(((f::$name))(u,p,t::Number) = (du=similar(u); f(du,u,p,t); du))
         push!(exprs,overloadex)
     end
 
     # export type constructor
     if gen_constructor
-        def_const_ex = :(($name)()) 
+        def_const_ex = :(($name)())
         push!(exprs,def_const_ex)
     end
 
@@ -135,7 +143,7 @@ function addodes!(rn::DiffEqBase.AbstractReactionNetwork; kwargs...)
     # functor for evaluating f
     functor_exprs = gentypefun_exprs(typeof(rn), esc_exprs=false, gen_constructor=false)
     eval( expr_arr_to_block(functor_exprs) )
-    
+
     nothing
 end
 
@@ -143,7 +151,7 @@ function addsdes!(rn::DiffEqBase.AbstractReactionNetwork)
     @unpack reactions, syms_to_ints, params_to_ints, scale_noise = rn
 
     # first construct an ODE reaction network
-    if rn.f == nothing 
+    if rn.f == nothing
         addodes!(rn)
     end
 
@@ -156,17 +164,17 @@ function addsdes!(rn::DiffEqBase.AbstractReactionNetwork)
     nothing
 end
 
-function addjumps!(rn::DiffEqBase.AbstractReactionNetwork; 
-                                    build_jumps=true, 
+function addjumps!(rn::DiffEqBase.AbstractReactionNetwork;
+                                    build_jumps=true,
                                     build_regular_jumps=true,
                                     minimal_jumps=false)
 
     @unpack reactions, syms_to_ints, params_to_ints = rn
 
     # parse the jumps
-    (jump_rate_expr, jump_affect_expr, jumps, regular_jumps) = get_jumps(reactions, 
-                                                                    syms_to_ints, 
-                                                                    params_to_ints; 
+    (jump_rate_expr, jump_affect_expr, jumps, regular_jumps) = get_jumps(reactions,
+                                                                    syms_to_ints,
+                                                                    params_to_ints;
                                                                     minimal_jumps=minimal_jumps)
 
     rn.jump_rate_expr   = jump_rate_expr
