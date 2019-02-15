a19d59f2f0e002a38d4eb8103d992c74a8392978
Gaussia <torkel.loman@gmail.com>
2019-02-15 11:34:04 +0000

    - Added tests for the parameter jacobian.

--- test/misc_tests.jl
+++ test/misc_tests.jl
@@ -30,3 +30,25 @@ for i = 1:100
     network3.jac(J,u,p,0.)
     @test maximum(J .- [-1-2*p[3]*u[2]*u[1] -p[3]*u[1]*u[1] 1.; -2*p[3]*u[2]*u[1] -1-p[3]*u[1]*u[1] 1; 2*p[3]*u[2]*u[1] p[3]*u[1]*u[1] -1.])<0.0001
 end
+
+#Tests the parameter jacobian.
+network1 = @reaction_network begin
+    (p1,1.0),   ∅ ↔ X
+    (1.0,p2),   X ↔ Y
+    (p3,p4),    Y ↔ Z
+end p1 p2 p3 p4
+J = zeros(3,4)
+network1.paramjac(J,[2.,3.,4.],[2.,3.,4.,5.],0.)
+@test J == [1. 3. 0. 0.; -0. -3. -3. 4.; 0. 0. 3. -4.]
+
+network2 = @reaction_network begin
+    (1.,p1*p2),   ∅ ↔ X
+    (p3^3+p1,p2),   2X ↔ Y
+end p1 p2 p3
+J = zeros(2,3)
+for i = 1:100
+    u = 10*rand(2)
+    p = 10*rand(3)
+    network2.paramjac(J,u,p,0.)
+    @test maximum(J .- [-u[1]*p[2]-u[1]^2 -u[1]*p[1]+2*u[2] -6*p[3]^2*u[1]^2/2; u[1]^2/2 -u[2] 3*p[3]^2*u[1]^2/2])<0.0001
+end
