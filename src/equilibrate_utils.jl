65f0bcb99d0327ecbed15e18663e6e5b69f65cbc
Gaussia <torkel.loman@gmail.com>
2019-02-16 19:06:46 +0000

    - Small fixes.

--- src/equilibrate_utils.jl
+++ src/equilibrate_utils.jl
@@ -54,9 +54,23 @@ end
 function make_hc_template(reaction_network::DiffEqBase.AbstractReactionNetwork)
     check_polynomial(reaction_network)
     p_template = randn(ComplexF64, length(reaction_network.params))
-    f_template = DynamicPolynomials.subs.(reaction_network.equilibratium_polynomial, Ref(internal___polyvar___p => p_template))
+    pvars, pars = find_param_vars(reaction_network.equilibratium_polynomial,p_template)
+    f_template = DynamicPolynomials.subs.(reaction_network.equilibratium_polynomial, Ref(pvars => pars))
     result_template = HomotopyContinuation.solve(f_template, report_progress=false)
-    reaction_network.homotopy_continuation_template = (p_template,solutions(result_template))
+    reaction_network.homotopy_continuation_template = (pars,solutions(result_template))
+end
+
+#Finds all (parameter) varriables in a polynomial and returns them and their respective parameter value.
+function find_param_vars(polys,pars)
+    vars = union(variables.(polys)...)
+    vars_new = Vector{PolyVar{true}}(); pars_new = Vector{Complex{Float64}}();
+    for var in vars
+        if (length(var.name)>24)&&(var.name[1:23]=="internal___polyvar___p[")&&(var.name[end]==']')&&(!in(var,vars_new))
+            push!(vars_new,var)
+            push!(pars_new,pars[parse(Int64,var.name[24:end-1])])
+        end
+    end
+    return (vars_new,pars_new)
 end
 
 #
@@ -72,11 +86,11 @@ end
 #
 function steady_states(reaction_network::DiffEqBase.AbstractReactionNetwork,pars::Vector{Float64})
     (reaction_network.homotopy_continuation_template==nothing) ? make_hc_template(reaction_network) : check_polynomial(reaction_network)
-    result = HomotopyContinuation.solve(reaction_network.equilibratium_polynomial, reaction_network.homotopy_continuation_template[2], parameters=internal___polyvar___p, p₁=reaction_network.homotopy_continuation_template[1], p₀=Complex{Float64}.(pars))
+    pvars, pars = find_param_vars(reaction_network.equilibratium_polynomial,Complex{Float64}.(pars))
+    result = HomotopyContinuation.solve(reaction_network.equilibratium_polynomial, reaction_network.homotopy_continuation_template[2], parameters=pvars, p₁=reaction_network.homotopy_continuation_template[1], p₀=pars)
     filter(realsolutions(result)) do x
             all(xᵢ -> xᵢ ≥ -0.001, x)
     end
-    return result
 end
 
 #
