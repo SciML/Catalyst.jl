71038ea1147b19838f42c91429012ff9c79e45a6
Torkel <torkel.loman@gmail.com>
2019-06-12 17:35:42 +0100

    - Updated the tests to be a bit more extensive.

--- test/equilibrate_test_2.jl
+++ test/equilibrate_test_2.jl
@@ -8,7 +8,12 @@ for rn in reaction_networks_hill
         for i=1:length(fps)
             stab[i] ? (tend = 10) : (tend = -10)
             end_point = OrdinaryDiffEq.solve(ODEProblem(rn,fps[i],(0.,tend),p),Rosenbrock23())[end]
-            @test maximum(end_point-fps[i])<0.0001
+            @test maximum(abs.(end_point-fps[i]))<0.0001
+        end
+        start_points = map(amp->amp*rand(length(rn.syms)),[1.,10.,100.])
+        for sp in start_points
+            end_point = OrdinaryDiffEq.solve(ODEProblem(rn,sp,(0.,1000.),p),Rosenbrock23())[end]
+            @test any(map(fp->maximum(abs.(fp-end_point)),fps) .< 0.001)
         end
     end
 end
