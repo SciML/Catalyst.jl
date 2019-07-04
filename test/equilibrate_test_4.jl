4532c4fa69a045e884e2f5e508d20669445e8ca2
Torkel <torkel.loman@gmail.com>
2019-07-04 16:28:02 +0100

    added test.

--- test/equilibrate_test_4.jl
+++ test/equilibrate_test_4.jl
@@ -7,9 +7,11 @@ for rn in reaction_networks_standard
     range1 = 1.: rand(5.:10.)
     range2 = 1.: rand(5.:10.)
     for p in p_vals
-        plot(bifurcations(rn,p,param1,range_tupple));
-        plot(bifurcations_grid(rn,p,param1,range1));
-        plot(bifurcations_grid_2d(rn,p,param1,range1,param2,range2));
-        plot(bifurcations_diagram_grid(rn,p,param1,range1,param2,range_tupple));
+        bif1 = bifurcations(rn,p,param1,range_tupple)
+        bif2 = bifurcations(rn,p,param1,range_tupple,solver=HcBifurcationSolverSimple)
+        bif_grid = bifurcations_grid(rn,p,param1,range1)
+        bif_grid_2d = bifurcations_grid_2d(rn,p,param1,range1,param2,range2)
+        bif_grid_dia = bifurcations_diagram_grid(rn,p,param1,range1,param2,range_tupple)
+        plot(bif1); plot(bif2); plot(bif_grid); plot(bif_grid_2d); plot(bif_grid_dia);
     end
 end
