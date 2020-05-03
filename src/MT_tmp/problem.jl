2fe6a090b87da4e5b2cc660db9b24580a1b85548
Torkel <torkel.loman@gmail.com>
2020-05-03 12:20:39 +0100

    - add missing args and kwargs in ODEProblem.

--- src/MT_tmp/problem.jl
+++ src/MT_tmp/problem.jl
@@ -1,7 +1,7 @@
 ### ODEProblem from AbstractReactionNetwork ###
 function MT_ODEProblem(rn::DiffEqBase.AbstractReactionNetwork, u0::Union{AbstractArray, Number}, tspan, p, args...; kwargs...)
     (rn.ode_system == nothing) && (rn.ode_system = convert(ODESystem,rn.reaction_system))
-    return ODEProblem(rn.ode_system,Pair.(rn.reaction_system.states,u0),tspan,Pair.(rn.reaction_system.ps,p))
+    return ODEProblem(rn.ode_system,Pair.(rn.reaction_system.states,u0),tspan,Pair.(rn.reaction_system.ps,p), args...; kwargs...)
 end
 
 ### SDEProblem from AbstractReactionNetwork ###
