43be3df6069fc9abde784b1e822d8be2a82ba996
Torkel <torkel.loman@gmail.com>
2020-05-04 14:40:40 +0100

    - Bigfix, SDEProblems should work now.

--- src/MT_tmp/problem.jl
+++ src/MT_tmp/problem.jl
@@ -8,5 +8,5 @@ end
 function MT_SDEProblem(rn::DiffEqBase.AbstractReactionNetwork, u0::Union{AbstractArray, Number}, tspan, p, args...; kwargs...)
     (rn.sde_system == nothing) && (rn.sde_system = convert(SDESystem,rn.reaction_system))
     p_matrix = zeros(length(rn.reaction_system.states), length(rn.reaction_system.eqs))
-    SDEProblem(rn.sde_system,Pair.(rn.reaction_system.states,u0),tspan,Pair.(rn.reaction_system.ps,p),args; noise_rate_prototype=p_matrix,kwargs...)
+    return SDEProblem(rn.sde_system,Pair.(rn.reaction_system.states,u0),tspan,Pair.(rn.reaction_system.ps,p),args...; noise_rate_prototype=p_matrix,kwargs...)
 end
