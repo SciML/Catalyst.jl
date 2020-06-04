a39c13f820c45e7eba0c2e1ee050471d3f1de86f
Torkel <torkel.loman@gmail.com>
2020-06-04 23:03:44 +0100

    Creating a new runtest file. A little bit uncertain of how the groups work, but think something like this.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,58 @@
-using SafeTestsets
+### Fecth the require packages ###
+using DiffEqBiological, ModelingToolkit
+using OrdinaryDiffEq, StochasticDiffEq, DiffEqJump
+using Test, SafeTestsets
+using LinearAlgebra, Random, Statistics
+using SparseArrays
 
+
+### Decalre constants realted to groups.
 const GROUP = get(ENV, "GROUP", "All")
 const is_APPVEYOR = Sys.iswindows() && haskey(ENV,"APPVEYOR")
 const is_TRAVIS = haskey(ENV,"TRAVIS")
 
-# full macro tests
+
+
+### Ruyn the tests ###
 @time begin
+
+# Standard tests realting to the model.
+if GROUP == "All" || GROUP == "Core"
+  @time @safetestset "" begin include(".jl") end
+  @time @safetestset "" begin include(".jl") end
+end
+
+# Tests relating to modification an already created model.
+if GROUP == "All" || GROUP == "Model Modifcations"
+  @time @safetestset "" begin include(".jl") end
+  @time @safetestset "" begin include(".jl") end
+end
+
+# Tests related to solving Ordinary Differential Equations.
+if GROUP == "All" || GROUP == "ODE Problems"
+  @time @safetestset "" begin include(".jl") end
+  @time @safetestset "" begin include(".jl") end
+end
+
+# Tests related to solving Stochastic Differential Equations.
+if GROUP == "All" || GROUP == "SDE Problems"
+  @time @safetestset "" begin include(".jl") end
+  @time @safetestset "" begin include(".jl") end
+end
+
+# Tests related to solvingJump Systems.
+if GROUP == "All" || GROUP == "Jump Problems"
+  @time @safetestset "" begin include(".jl") end
+  @time @safetestset "" begin include(".jl") end
+end
+
+# Miscellaneous tests.
+if GROUP == "All" || GROUP == "Miscellaneous"
+  @time @safetestset "" begin include(".jl") end
+  @time @safetestset "" begin include(".jl") end
+end
+
+
 
 if GROUP == "All" || GROUP == "Core"
   @time @safetestset "Model Macro" begin include("make_model_test.jl") end
