3cde53e4ccd7ecb50cf780cc56b3833724309c83
Samuel Isaacson <isaacsas@users.noreply.github.com>
2020-07-20 10:35:28 -0400

    fix non-mathjax output

--- test/latexify.jl
+++ test/latexify.jl
@@ -53,6 +53,25 @@ raw"\begin{align}
 \end{align}
 "
 
+@test latexify(r, mathjax = false) == 
+raw"\begin{align}
+\ce{ \varnothing &->[$\frac{v1 K1^{n1}}{K1^{n1} + \left( \mathrm{X2}\left( t \right) \right)^{n1}} \frac{v1 \left( \mathrm{X4}\left( t \right) \right)^{n1}}{K1^{n1} + \left( \mathrm{X4}\left( t \right) \right)^{n1}}$] X1}\\
+\ce{ \varnothing &->[$\frac{v2 \left( \mathrm{X5}\left( t \right) \right)^{n2}}{K2^{n2} + \left( \mathrm{X5}\left( t \right) \right)^{n2}}$] X2}\\
+\ce{ \varnothing &->[$\frac{v3 \left( \mathrm{X3}\left( t \right) \right)^{n3}}{K3^{n3} + \left( \mathrm{X3}\left( t \right) \right)^{n3}}$] X3}\\
+\ce{ \varnothing &->[$\frac{v4 K4^{n4}}{K4^{n4} + \left( \mathrm{X1}\left( t \right) \right)^{n4}}$] X4}\\
+\ce{ \varnothing &->[$\frac{v5 \left( \mathrm{X2}\left( t \right) \right)^{n5}}{K5^{n5} + \left( \mathrm{X2}\left( t \right) \right)^{n5}}$] X5}\\
+\ce{ X2 &<=>[$k1$][$k2$] X1 + 2 X4}\\
+\ce{ X4 &<=>[$k3$][$k4$] X3}\\
+\ce{ 3 X5 + X1 &<=>[$k5$][$k6$] X2}\\
+\ce{ X1 &->[$d1$] \varnothing}\\
+\ce{ X2 &->[$d2$] \varnothing}\\
+\ce{ X3 &->[$d3$] \varnothing}\\
+\ce{ X4 &->[$d4$] \varnothing}\\
+\ce{ X5 &->[$d5$] \varnothing}
+\end{align}
+"
+
+
 r = @reaction_network begin
     (hill(B, p_a, k, n), d_a), 0 ↔ A
     (p_b, d_b), 0 ↔ B
@@ -65,6 +84,14 @@ raw"\begin{align}
 \ce{ \varnothing &<=>[\frac{p_{a} \left( \mathrm{B}\left( t \right) \right)^{n}}{k^{n} + \left( \mathrm{B}\left( t \right) \right)^{n}}][d_{a}] A}\\
 \ce{ \varnothing &<=>[p_{b}][d_{b}] B}\\
 \ce{ 3 B &<=>[r_{a}][r_{b}] A}
+\end{align}
+"
+
+@test latexify(r, mathjax = false) == 
+raw"\begin{align}
+\ce{ \varnothing &<=>[$\frac{p_{a} \left( \mathrm{B}\left( t \right) \right)^{n}}{k^{n} + \left( \mathrm{B}\left( t \right) \right)^{n}}$][$d_{a}$] A}\\
+\ce{ \varnothing &<=>[$p_{b}$][$d_{b}$] B}\\
+\ce{ 3 B &<=>[$r_{a}$][$r_{b}$] A}
 \end{align}
 "
 
