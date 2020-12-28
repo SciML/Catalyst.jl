94803972491c56686ae3628256fe942cc4c81810
Samuel Isaacson <isaacsas@users.noreply.github.com>
2020-12-28 16:16:31 -0500

    tweak tests

--- test/custom_functions.jl
+++ test/custom_functions.jl
@@ -3,10 +3,6 @@ using DiffEqBase, Catalyst, Random, Test
 
 
 ### Tests various cutom made functions ###
-
-# @reaction_func new_hill(x, v, k, n) = v*x^n/(k^n+x^n)
-# @reaction_func new_poly(x,p1,p2) = p1*x^2+p2
-# @reaction_func new_exp(x,p) = exp(-p*x)
 new_hill(x, v, k, n) = v*x^n/(k^n+x^n)
 @register new_hill(x,v,k,n)
 new_poly(x,p1,p2) = p1*x^2+p2
@@ -37,9 +33,9 @@ f2 = ODEFunction(convert(ODESystem,custom_function_network_2),jac=true)
 g1 = SDEFunction(convert(SDESystem,custom_function_network_1))
 g2 = SDEFunction(convert(SDESystem,custom_function_network_2))
 for factor in [1e-2, 1e-1, 1e0, 1e1, 1e2, 1e3]
-    local u0 = factor*rand(length(custom_function_network_1.states))
-    local p = factor*rand(length(custom_function_network_2.ps))
-    local t = rand()
+    u0 = factor*rand(length(custom_function_network_1.states))
+    p = factor*rand(length(custom_function_network_2.ps))
+    t = rand()
     @test all(abs.(f1(u0,p,t) .- f2(u0,p,t)) .< 100*eps())
     @test all(abs.(f1.jac(u0,p,t) .- f2.jac(u0,p,t)) .< 100*eps())
     @test all(abs.(g1(u0,p,t) .- g2(u0,p,t)) .< 100*eps())
