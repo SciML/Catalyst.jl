2e4af6f12f53526e7602e149d2afb584eef42558
Samuel Isaacson <isaacsas@users.noreply.github.com>
2021-01-22 12:43:53 -0500

    fix latexify tests

--- test/latexify.jl
+++ test/latexify.jl
@@ -38,53 +38,6 @@ end v1 K1 n1 v2 K2 n2 v3 K3 n3 v4 K4 n4 v5 K5 n5 k1 k2 k3 k4 k5 k6 d1 d2 d3 d4 d
 @test latexify(r) == replace(
 raw"\begin{align}
 \require{mhchem}
-\ce{ \varnothing &->[\frac{v1 K1^{n1}}{K1^{n1} + \left( \mathrm{X2}\left( t \right) \right)^{n1}} \frac{v1 \left( \mathrm{X4}\left( t \right) \right)^{n1}}{K1^{n1} + \left( \mathrm{X4}\left( t \right) \right)^{n1}}] X1}\\
-\ce{ \varnothing &->[\frac{v2 \left( \mathrm{X5}\left( t \right) \right)^{n2}}{K2^{n2} + \left( \mathrm{X5}\left( t \right) \right)^{n2}}] X2}\\
-\ce{ \varnothing &->[\frac{v3 \left( \mathrm{X3}\left( t \right) \right)^{n3}}{K3^{n3} + \left( \mathrm{X3}\left( t \right) \right)^{n3}}] X3}\\
-\ce{ \varnothing &->[\frac{v4 K4^{n4}}{K4^{n4} + \left( \mathrm{X1}\left( t \right) \right)^{n4}}] X4}\\
-\ce{ \varnothing &->[\frac{v5 \left( \mathrm{X2}\left( t \right) \right)^{n5}}{K5^{n5} + \left( \mathrm{X2}\left( t \right) \right)^{n5}}] X5}\\
-\ce{ X2 &<=>[k1][k2] X1 + 2 X4}\\
-\ce{ X4 &<=>[k3][k4] X3}\\
-\ce{ 3 X5 + X1 &<=>[k5][k6] X2}\\
-\ce{ X1 &->[d1] \varnothing}\\
-\ce{ X2 &->[d2] \varnothing}\\
-\ce{ X3 &->[d3] \varnothing}\\
-\ce{ X4 &->[d4] \varnothing}\\
-\ce{ X5 &->[d5] \varnothing}
-\end{align}
-", "\r\n"=>"\n")
-
-
-# Latexify.@generate_test latexify(r, mathjax=false)
-@test latexify(r, mathjax = false) == replace(
-raw"\begin{align}
-\ce{ \varnothing &->[$\frac{v1 K1^{n1}}{K1^{n1} + \left( \mathrm{X2}\left( t \right) \right)^{n1}} \frac{v1 \left( \mathrm{X4}\left( t \right) \right)^{n1}}{K1^{n1} + \left( \mathrm{X4}\left( t \right) \right)^{n1}}$] X1}\\
-\ce{ \varnothing &->[$\frac{v2 \left( \mathrm{X5}\left( t \right) \right)^{n2}}{K2^{n2} + \left( \mathrm{X5}\left( t \right) \right)^{n2}}$] X2}\\
-\ce{ \varnothing &->[$\frac{v3 \left( \mathrm{X3}\left( t \right) \right)^{n3}}{K3^{n3} + \left( \mathrm{X3}\left( t \right) \right)^{n3}}$] X3}\\
-\ce{ \varnothing &->[$\frac{v4 K4^{n4}}{K4^{n4} + \left( \mathrm{X1}\left( t \right) \right)^{n4}}$] X4}\\
-\ce{ \varnothing &->[$\frac{v5 \left( \mathrm{X2}\left( t \right) \right)^{n5}}{K5^{n5} + \left( \mathrm{X2}\left( t \right) \right)^{n5}}$] X5}\\
-\ce{ X2 &<=>[$k1$][$k2$] X1 + 2 X4}\\
-\ce{ X4 &<=>[$k3$][$k4$] X3}\\
-\ce{ 3 X5 + X1 &<=>[$k5$][$k6$] X2}\\
-\ce{ X1 &->[$d1$] \varnothing}\\
-\ce{ X2 &->[$d2$] \varnothing}\\
-\ce{ X3 &->[$d3$] \varnothing}\\
-\ce{ X4 &->[$d4$] \varnothing}\\
-\ce{ X5 &->[$d5$] \varnothing}
-\end{align}
-", "\r\n"=>"\n")
-
-
-r = @reaction_network begin
-    (hill(B, p_a, k, n), d_a), 0 ↔ A
-    (p_b, d_b), 0 ↔ B
-    (r_a, r_b), 3B ↔ A
-end p_a k n d_a p_b d_b r_a r_b
-
-# Latexify.@generate_test latexify(r)
-@test latexify(r) == replace(
-raw"\begin{align}
-\require{mhchem}
 \ce{ \varnothing &->[K1^{n1} v1^{2} \left( \mathrm{X4}\left( t \right) \right)^{n1} \mathrm{inv}\left( K1^{n1} + \left( \mathrm{X2}\left( t \right) \right)^{n1} \right) \mathrm{inv}\left( K1^{n1} + \left( \mathrm{X4}\left( t \right) \right)^{n1} \right)] X1}\\
 \ce{ \varnothing &->[v2 \left( \mathrm{X5}\left( t \right) \right)^{n2} \mathrm{inv}\left( K2^{n2} + \left( \mathrm{X5}\left( t \right) \right)^{n2} \right)] X2}\\
 \ce{ \varnothing &->[v3 \left( \mathrm{X3}\left( t \right) \right)^{n3} \mathrm{inv}\left( K3^{n3} + \left( \mathrm{X3}\left( t \right) \right)^{n3} \right)] X3}\\
@@ -119,10 +72,30 @@ raw"\begin{align}
 \ce{ X5 &->[$d5$] \varnothing}
 \end{align}
 ", "\r\n"=>"\n")
+
+
+r = @reaction_network begin
+    (hill(B, p_a, k, n), d_a), 0 ↔ A
+    (p_b, d_b), 0 ↔ B
+    (r_a, r_b), 3B ↔ A
+end p_a k n d_a p_b d_b r_a r_b
+
+# Latexify.@generate_test latexify(r)
+@test latexify(r) == replace(
+raw"\begin{align}
+\require{mhchem}
+\ce{ \varnothing &<=>[p_{a} \left( B\left( t \right) \right)^{n} \mathrm{inv}\left( k^{n} + \left( B\left( t \right) \right)^{n} \right)][d_{a}] A}\\
+\ce{ \varnothing &<=>[p_{b}][d_{b}] B}\\
+\ce{ 3 B &<=>[r_{a}][r_{b}] A}
+\end{align}
+", "\r\n"=>"\n")
+
+# Latexify.@generate_test latexify(r, mathjax=false)
 @test latexify(r, mathjax = false) == replace(
 raw"\begin{align}
-\ce{ \varnothing &<=>[$\frac{p_{a} \left( B\left( t \right) \right)^{n}}{k^{n} + \left( B\left( t \right) \right)^{n}}$][$d_{a}$] A}\\
+\ce{ \varnothing &<=>[$p_{a} \left( B\left( t \right) \right)^{n} \mathrm{inv}\left( k^{n} + \left( B\left( t \right) \right)^{n} \right)$][$d_{a}$] A}\\
 \ce{ \varnothing &<=>[$p_{b}$][$d_{b}$] B}\\
 \ce{ 3 B &<=>[$r_{a}$][$r_{b}$] A}
 \end{align}
 ", "\r\n"=>"\n")
+
