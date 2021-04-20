419a6cacdbaab3b99c7e6a3e32847447b8063176
Torkel <torkel.loman@gmail.com>
2021-04-20 16:14:56 +0200

    reduce_test_limit

--- test/custom_functions.jl
+++ test/custom_functions.jl
@@ -10,23 +10,23 @@ new_poly(x,p1,p2) = .5*p1*x^2
 new_exp(x,p) = exp(-p*x)
 
 custom_function_network_1 = @reaction_network begin
-    hill(X,v1,K1,2), X + Y --> Z1
-    mm(X,v2,K2), X + Y --> Z2
-    p1*X^2+p2, X + Y --> Z3
-    exp(-p3*Y), X + Y --> Z4
-    hillR(X,v3,K3,2), X + Y --> Z5
-    mmR(X,v4,K4), X + Y --> Z6
-    hillAR(X,Y,v5,K5,2), X + Y --> Z7
+    hill(X1,v1,K1,2), X1 + Y1--> Z1
+    mm(X2,v2,K2), X2 + Y2 --> Z2
+    p1*X3^2+p2, X3 + Y3 --> Z3
+    exp(-p3*Y4), X4 + Y4 --> Z4
+    hillR(X5,v3,K3,2), X5 + Y5 --> Z5
+    mmR(X6,v4,K4), X6 + Y6 --> Z6
+    hillAR(X7,Y7,v5,K5,2), X7 + Y7 --> Z7
 end v1 K1 v2 K2 p1 p2 p3 v3 K3 v4 K4 v5 K5
 
 custom_function_network_2 = @reaction_network begin
-    new_hill(X,v1,K1,2), X + Y --> Z1
-    v2*X/(X+K2), X + Y --> Z2
-    2*new_poly(X,p1,p2)+p2, X + Y --> Z3
-    new_exp(Y,p3), X + Y --> Z4
-    v3*(K3^2)/(K3^2+X^2), X + Y --> Z5
-    v4*K4/(X+K4), X + Y --> Z6
-    v5*(X^2)/(K5^2+X^2+Y^2), X + Y --> Z7
+    new_hill(X1,v1,K1,2), X1 + Y1 --> Z1
+    v2*X2/(X2+K2), X2 + Y2 --> Z2
+    2*new_poly(X3,p1,p2)+p2, X3 + Y3 --> Z3
+    new_exp(Y4,p3), X4 + Y4--> Z4
+    v3*(K3^2)/(K3^2+X5^2), X5 + Y5 --> Z5
+    v4*K4/(X6+K4), X6 + Y6 --> Z6
+    v5*(X7^2)/(K5^2+X7^2+Y7^2), X7 + Y7 --> Z7
 end v1 K1 v2 K2 p1 p2 p3 v3 K3 v4 K4 v5 K5
 
 f1 = ODEFunction(convert(ODESystem,custom_function_network_1),jac=true)
@@ -37,7 +37,7 @@ for factor in [1e-2, 1e-1, 1e0, 1e1, 1e2]
     u0 = factor*rand(rng,length(get_states(custom_function_network_1)))
     p = factor*rand(rng,length(get_ps(custom_function_network_2)))
     t = rand(rng)
-    @test all(abs.(f1(u0,p,t) .- f2(u0,p,t)) .< 10e-6)
-    @test all(abs.(f1.jac(u0,p,t) .- f2.jac(u0,p,t)) .< 10e-6)
-    @test all(abs.(g1(u0,p,t) .- g2(u0,p,t)) .< 10e-6)
+    @test all(abs.(f1(u0,p,t) .- f2(u0,p,t)) .< 10e-10)
+    @test all(abs.(f1.jac(u0,p,t) .- f2.jac(u0,p,t)) .< 10e-10)
+    @test all(abs.(g1(u0,p,t) .- g2(u0,p,t)) .< 10e-10)
 end
