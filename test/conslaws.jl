8a038371622112e05eee08cea08b2bd365787652
kaandocal <26488673+kaandocal@users.noreply.github.com>
2021-06-03 12:17:17 +0100

    Added more tests for conservation law detection

--- test/conslaws.jl
+++ test/conslaws.jl
@@ -1,4 +1,7 @@
 using Catalyst, Test
+using LinearAlgebra
+
+include("test_networks.jl")
 
 rn = @reaction_network begin
     (1, 2), A + B <--> C 
@@ -25,3 +28,20 @@ D = [ 1 -1 0 0 0 0 0 0 0 0;
       0  1 1 0 0 0 0 0 0 0 ]
 
 @test any(D[j,:] == C[i,:] for i in 1:size(C,1), j in size(D,1))
+
+Ss_standard = map(netstoichmat, reaction_networks_standard)
+Cs_standard = map(conservationlaws, Ss_standard)
+@test all(size(C, 1) == 0 for C in Cs_standard)
+
+Ss_hill = map(netstoichmat, reaction_networks_hill)
+Cs_hill = map(conservationlaws, Ss_hill)
+@test all(size(C, 1) == 0 for C in Cs_hill)
+
+function consequiv(A, B)
+    rank([A; B]) == rank(A) == rank(B)
+end
+
+Ss_constraint = map(netstoichmat, reaction_networks_constraint)
+Cs_constraint = map(conservationlaws, Ss_constraint)
+
+@test all(consequiv.(Matrix{Int}.(Cs_constraint), reaction_network_constraints))
