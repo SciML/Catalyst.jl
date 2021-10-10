345b4f4255ce9153869a9dedc736a368a87a3d28
Sam Isaacson <isaacsas@users.noreply.github.com>
2021-10-10 15:36:20 -0400

    export reactionparamsmap

--- src/Catalyst.jl
+++ src/Catalyst.jl
@@ -58,7 +58,7 @@ export mm, mmr, hill, hillr, hillar
 
 # functions to query network properties
 include("networkapi.jl")
-export species, reactionparams, reactions, speciesmap, paramsmap, numspecies, numreactions, numreactionparams
+export species, reactionparams, reactions, speciesmap, paramsmap, reactionparamsmap, numspecies, numreactions, numreactionparams
 export make_empty_network, addspecies!, addparam!, addreaction!
 export dependants, dependents, substoichmat, prodstoichmat, netstoichmat
 export conservationlaws, conservedquantities
