49b54873f4c2c9fea89a3bed3c3cb2bc3ddb3bdc
Sam Isaacson <isaacsas@users.noreply.github.com>
2021-10-19 20:00:31 -0400

    switch LightGraphs to Graphs

--- src/Catalyst.jl
+++ src/Catalyst.jl
@@ -20,10 +20,9 @@ import ModelingToolkit: get_variables, namespace_expr, namespace_equation, get_v
 import ModelingToolkit: check_variables, check_parameters, _iszero, _merge, check_units, get_unit
 
 import Base: (==), hash, size, getindex, setindex, isless, Sort.defalg, length, show
-import MacroTools, LightGraphs, AbstractAlgebra
+import MacroTools, Graphs, AbstractAlgebra
 
 # globals for the modulate
-const LG = LightGraphs
 const AA = AbstractAlgebra
 const DEFAULT_IV = (@parameters t)[1]
 
