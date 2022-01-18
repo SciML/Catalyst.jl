0bc4d0e9b53f49535db9c8d074e2611d3f01e673
Sam Isaacson <isaacsas@users.noreply.github.com>
2022-01-18 12:36:59 -0500

    Update src/reactionsystem.jl

--- src/reactionsystem.jl
+++ src/reactionsystem.jl
@@ -253,7 +253,6 @@ function ReactionSystem(eqs, iv, states, ps;
     MT.collect_var_to_name!(var_to_name, (eq.lhs for eq in observed))
 
     if constraints !== nothing
-
         MT.process_variables!(var_to_name, defaults, get_ps(constraints))
         MT.collect_var_to_name!(var_to_name, get_states(constraints))
     end
