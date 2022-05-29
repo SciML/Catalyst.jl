59c4d8ee417b7ac9c5a6ca75759ddb89050b8102
Sam Isaacson <isaacsas@users.noreply.github.com>
2022-05-29 16:11:13 -0400

    tweak doc string

--- src/networkapi.jl
+++ src/networkapi.jl
@@ -636,6 +636,9 @@ end
     incidencemat(rn::ReactionSystem; sparse=false)
 
 Calculate the incidence matrix of `rn`, see [`reactioncomplexes`](@ref).
+
+Notes:
+- Is cached in `rn` so that future calls, assuming the same sparsity, will also be fast.
 """
 incidencemat(rn::ReactionSystem; sparse=false) = reactioncomplexes(rn; sparse)[2]
 
