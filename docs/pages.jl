c2ce0add1cb7e57ca6f777ab212a12e453806a16
Christopher Rackauckas <accounts@chrisrackauckas.com>
2022-06-02 11:19:21 -0400

    Create pages.jl

---
+++ docs/pages.jl
@@ -0,0 +1,18 @@
+pages = Any[
+        "Home" => "index.md",
+        "Tutorials" => Any[
+            "tutorials/using_catalyst.md",
+            "tutorials/dsl.md",
+            "tutorials/reaction_systems.md",
+            "tutorials/basic_examples.md",
+            "tutorials/compositional_modeling.md",
+            "tutorials/symbolic_stoich.md",
+            "tutorials/bifurcation_diagram.md",
+            "tutorials/parameter_estimation.md",
+            "tutorials/generating_reactions_programmatically.md"
+        ],
+        "FAQs" => "faqs.md",
+        "API" => Any[
+            "api/catalyst_api.md"
+        ]
+    ]
