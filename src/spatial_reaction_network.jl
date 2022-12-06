d04abf38ced515f22fcfdd207cbb878a1b47cf7a
Torkel Loman <torkel.loman@gmail.com>
2022-12-06 16:34:12 -0500

    Basic formatting

--- src/spatial_reaction_network.jl
+++ src/spatial_reaction_network.jl
@@ -5,28 +5,28 @@ struct SpatialReaction
     """The rate function (excluding mass action terms). Currentl only cosntants supported"""
     rate::Any
     """Reaction substrates (source and destination)."""
-    substrates::Tuple{Vector,Vector}
+    substrates::Tuple{Vector, Vector}
     """Reaction products (source and destination)."""
-    products::Tuple{Vector,Vector}
+    products::Tuple{Vector, Vector}
     """The stoichiometric coefficients of the reactants (source and destination)."""
-    substoich::Tuple{Vector{Int64},Vector{Int64}}
+    substoich::Tuple{Vector{Int64}, Vector{Int64}}
     """The stoichiometric coefficients of the products (source and destination)."""
-    prodstoich::Tuple{Vector{Int64},Vector{Int64}}
+    prodstoich::Tuple{Vector{Int64}, Vector{Int64}}
     """The net stoichiometric coefficients of all species changed by the reaction (source and destination)."""
-    netstoich::Tuple{Vector{Pair},Vector{Pair}}
+    netstoich::Tuple{Vector{Pair}, Vector{Pair}}
     """
     `false` (default) if `rate` should be multiplied by mass action terms to give the rate law.
     `true` if `rate` represents the full reaction rate law.
     Currently only `false`, is supported.
     """
     only_use_rate::Bool
-    function SpatialReaction(rate, substrates, products, substoich, prodstoich; only_use_rate=false)
-    products::Tuple{Vector,Vector}
-        new(rate, substrates, products, substoich, prodstoich, get_netstoich.(substrates,products,substoich,prodstoich), only_use_rate)
+    function SpatialReaction(rate, substrates, products, substoich, prodstoich;
+                             only_use_rate = false)
+        products::Tuple{Vector, Vector}
+        new(rate, substrates, products, substoich, prodstoich,
+            get_netstoich.(substrates, products, substoich, prodstoich), only_use_rate)
     end
 end
-
-
 
 ### Lattice Reaction Network Structure ###
 # Couples:
@@ -43,7 +43,7 @@ struct LatticeReactionSystem # <: MT.AbstractTimeDependentSystem # Adding this p
     lattice::DiGraph
     """Dependent (state) variables representing amount of each species. Must not contain the
     independent variable."""
-    
+
     function LatticeReactionSystem(rs, spatial_reactions, lattice::DiGraph)
         return new(rs, spatial_reactions, lattice)
     end
@@ -57,8 +57,8 @@ end
 function DiffEqBase.ODEProblem(lrs::LatticeReactionSystem, u0, tspan,
                                p = DiffEqBase.NullParameters(), args...;
                                kwargs...)
-    @unpack rs, spatial_reactions, lattice = lrs       
-                        
+    @unpack rs, spatial_reactions, lattice = lrs
+
     spatial_params = unique(getfield.(spatial_reactions, :rate))
     pV_in, pE_in = split_parameters(p, spatial_params)
     nV, nE = length.([vertices(lattice), edges(lattice)])
@@ -68,36 +68,51 @@ function DiffEqBase.ODEProblem(lrs::LatticeReactionSystem, u0, tspan,
 
     u0 = matrix_form(u0, nV, u_idxs)
     pV = matrix_form(pV_in, nV, pV_idxes)
-    pE = matrix_form(pE_in, nE, pE_idxes);
+    pE = matrix_form(pE_in, nE, pE_idxes)
 
-    return ODEProblem(build_f(lrs, u_idxs, pE_idxes), u0, tspan, (pV,pE), args...; kwargs...)
+    return ODEProblem(build_f(lrs, u_idxs, pE_idxes), u0, tspan, (pV, pE), args...;
+                      kwargs...)
 end
 
 # Splits parameters into those for the compartments and those for the connections.
 split_parameters(parameters::Tuple, spatial_params) = parameters
-split_parameters(parameters::Vector, spatial_params) = filter(p -> !in(p[1], spatial_params), parameters), filter(p -> in(p[1], spatial_params), parameters);
+function split_parameters(parameters::Vector, spatial_params)
+    filter(p -> !in(p[1], spatial_params), parameters),
+    filter(p -> in(p[1], spatial_params), parameters)
+end;
 
 # Converts species and parameters to matrices form.
 matrix_form(input::Matrix, args...) = input
-matrix_form(input::Vector{Pair{Symbol,Vector{Float64}}}, n, index_dict) = mapreduce(permutedims, vcat, last.(sort(input, by=i -> index_dict[i[1]])))
-matrix_form(input::Vector, n, index_dict) = matrix_form(map(i -> (i[2] isa Vector) ? i[1] => i[2] : i[1] => fill(i[2], n), input), n, index_dict);
+function matrix_form(input::Vector{Pair{Symbol, Vector{Float64}}}, n, index_dict)
+    mapreduce(permutedims, vcat, last.(sort(input, by = i -> index_dict[i[1]])))
+end
+function matrix_form(input::Vector, n, index_dict)
+    matrix_form(map(i -> (i[2] isa Vector) ? i[1] => i[2] : i[1] => fill(i[2], n), input),
+                n, index_dict)
+end;
 
 # Creates a function for simulating the spatial ODE with spatial reactions.
-function build_f(lrs::LatticeReactionSystem, u_idxs::Dict{Symbol,Int64}, pE_idxes::Dict{Symbol,Int64})
+function build_f(lrs::LatticeReactionSystem, u_idxs::Dict{Symbol, Int64},
+                 pE_idxes::Dict{Symbol, Int64})
     ofunc = ODEFunction(convert(ODESystem, lrs.rs))
 
     return function internal___spatial___f(du, u, p, t)
         # Updates for non-spatial reactions.
-        for comp_i in 1:size(u,2)
-            ofunc((@view du[:,comp_i]), (@view u[:,comp_i]), p[1], t)
+        for comp_i in 1:size(u, 2)
+            ofunc((@view du[:, comp_i]), (@view u[:, comp_i]), p[1], t)
         end
-        
+
         # Updates for spatial reactions.
-        for comp_i in 1:size(u,2)
-            for comp_j::Int64 in (lrs.lattice.fadjlist::Vector{Vector{Int64}})[comp_i], sr::SpatialReaction in lrs.spatial_reactions::Vector{SpatialReaction}
-                rate = get_rate(sr,p[2],(@view u[:,comp_i]),(@view u[:,comp_j]), u_idxs, pE_idxes)
-                foreach(stoich -> du[u_idxs[stoich[1]],comp_i] += rate*stoich[2], sr.netstoich[1])
-                foreach(stoich -> du[u_idxs[stoich[1]],comp_j] += rate*stoich[2], sr.netstoich[2])
+        for comp_i in 1:size(u, 2)
+            for comp_j::Int64 in (lrs.lattice.fadjlist::Vector{Vector{Int64}})[comp_i],
+                sr::SpatialReaction in lrs.spatial_reactions::Vector{SpatialReaction}
+
+                rate = get_rate(sr, p[2], (@view u[:, comp_i]), (@view u[:, comp_j]),
+                                u_idxs, pE_idxes)
+                foreach(stoich -> du[u_idxs[stoich[1]], comp_i] += rate * stoich[2],
+                        sr.netstoich[1])
+                foreach(stoich -> du[u_idxs[stoich[1]], comp_j] += rate * stoich[2],
+                        sr.netstoich[2])
             end
         end
     end
@@ -106,15 +121,11 @@ end
 # Get the rate of a specific reaction.
 function get_rate(sr, pE, u_src, u_dst, u_idxs, pE_idxes)
     product = pE[pE_idxes[sr.rate]]
-    !isempty(sr.substrates[1]) && (product *= prod(u_src[u_idxs[subs[1]]]^subs[2] / factorial(subs[2]) for subs in Pair.(sr.substrates[1],sr.substoich[1])))
-    !isempty(sr.substrates[2]) && (product *= prod(u_dst[u_idxs[subs[1]]]^subs[2] / factorial(subs[2]) for subs in Pair.(sr.substrates[2],sr.substoich[2])))
+    !isempty(sr.substrates[1]) &&
+        (product *= prod(u_src[u_idxs[subs[1]]]^subs[2] / factorial(subs[2])
+                         for subs in Pair.(sr.substrates[1], sr.substoich[1])))
+    !isempty(sr.substrates[2]) &&
+        (product *= prod(u_dst[u_idxs[subs[1]]]^subs[2] / factorial(subs[2])
+                         for subs in Pair.(sr.substrates[2], sr.substoich[2])))
     return product::Float64
 end
-
-# Get the rate of a specific reaction.
-#function get_rate(sr, pE, u_src, u_dst, u_idxs, pE_idxes)
-#    product = pE[pE_idxes[sr.rate]]
-#    !isempty(sr.substrates[1]) && (product *= prod(u_src[u_idxs[subs[1]]]^subs[2] / factorial(subs[2]) for subs in sr.substrates[1]))
-#    !isempty(sr.substrates[2]) && (product *= prod(u_dst[u_idxs[subs[1]]]^subs[2] / factorial(subs[2]) for subs in sr.substrates[2]))
-#    return product::Float64
-#end
