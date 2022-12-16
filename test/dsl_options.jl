e58f0308ed94f346657c7e43b137b8898cd3455a
Torkel <torkel.loman@gmail.com>
2022-12-16 11:11:35 -0500

    Add more tests using default values.

--- test/dsl_options.jl
+++ test/dsl_options.jl
@@ -1,6 +1,6 @@
 #! format: off
 
-using Catalyst, ModelingToolkit
+using Catalyst, ModelingToolkit, OrdinaryDiffEq
 
 # Test creating networks with/without options.
 @reaction_network begin (k1, k2), A <--> B end
@@ -166,3 +166,44 @@ rn9 = @reaction_network name begin
     1, A --> B
     (d1, d2), (A, B) --> 0
 end
+
+
+rn10 = @reaction_network name begin
+@parameters p1=1.0 p2=2.0 k1=4.0 k2=5.0 v=8.0 K=9.0 n=3 d=10.0
+@species X=4.0 Y=3.0 X2Y=2.0 Z=1.0
+    (p1,p2), 0 --> (X,Y)
+    (k1,k2), 2X + Y --> X2Y
+    hill(X2Y,v,K,n), 0 --> Z
+    d, (X,Y,X2Y,Z) --> 0
+end
+u0_10 = []
+p_10 = []
+
+rn11 = @reaction_network name begin
+@parameters p1=1.0 p2 k1=4.0 k2 v=8.0 K n=3 d
+@species X=4.0 Y X2Y Z=1.0
+    (p1,p2), 0 --> (X,Y)
+    (k1,k2), 2X + Y --> X2Y
+    hill(X2Y,v,K,n), 0 --> Z
+    d, (X,Y,X2Y,Z) --> 0
+end
+u0_11 = [:p2=>2.0, :k2=>5.0, :K=>9.0, :d=>10.0]
+p_11 = [:Y=>3.0, :X2Y=>2.0]
+
+rn12 = @reaction_network name begin
+@parameters p1 p2 k1 k2 v K n d
+@species X Y Z X2Y
+    (p1,p2), 0 --> (X,Y)
+    (k1,k2), 2X + Y --> X2Y
+    hill(X2Y,v,K,n), 0 --> Z
+    d, (X,Y,X2Y,Z) --> 0
+end
+u0_12 = [:p1=>1.0, :p2=>2.0, :k1=>4.0, :k2=>5.0, :v=>8.0, :K=>9.0, :n=>3, :d=>10.0]
+p_12 = [:X=>4.0, :Y=>3.0, :X2Y=>2.0, :Z=>1.0]
+
+uEnd_10 = solve(ODEProblem(rn10,u0_10,(0.0,10.0),p_10),Rosenbrock23()).u[end]
+uEnd_11 = solve(ODEProblem(rn11,u0_11,(0.0,10.0),p_11),Rosenbrock23()).u[end]
+uEnd_12 = solve(ODEProblem(rn12,u0_12,(0.0,10.0),p_12),Rosenbrock23()).u[end]
+
+@test isapprox(uEnd_10, uEnd_11, rtol = 1e3 * eps())
+@test isapprox(uEnd_11, uEnd_12, rtol = 1e3 * eps())
