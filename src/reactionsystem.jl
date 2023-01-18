7e6213ea4831f83e33d59f71cbbcd034e1fbf846
Torkel <torkel.loman@gmail.com>
2023-01-18 12:10:49 -0500

    format

--- src/reactionsystem.jl
+++ src/reactionsystem.jl
@@ -627,7 +627,8 @@ function make_ReactionSystem_internal(rxs::Vector{<:Reaction}, iv, no_sps::Nothi
             (p isa Symbolics.Symbolic) && findvars!(ps, sts, p, ivs, vars)
         end
     end
-    ReactionSystem(rxs, t, collect(issetequal(sts,sts_in) ? sts_in : sts), collect(ps); spatial_ivs, kwargs...)
+    ReactionSystem(rxs, t, collect(issetequal(sts, sts_in) ? sts_in : sts), collect(ps);
+                   spatial_ivs, kwargs...)
 end
 
 function ReactionSystem(iv; kwargs...)
