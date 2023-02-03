23a83576289a515c8a0ee5e21a6748772a65dbda
Torkel <torkel.loman@gmail.com>
2023-02-03 14:48:45 -0500

    Add "exampel network" category.

--- docs/pages.jl
+++ docs/pages.jl
@@ -7,7 +7,6 @@ pages = Any[
             "Catalyst Functionality" => Any[
                                 "catalyst_functionality/dsl_description.md",
                                 "catalyst_functionality/programmatic_CRN_construction.md",
-                                "catalyst_functionality/basic_CRN_examples.md",
                                 "catalyst_functionality/compositional_modeling.md",
                                 "catalyst_functionality/parametric_stoichiometry.md"
                             ],
@@ -16,7 +15,10 @@ pages = Any[
                                 "tutorials/homotopy_continuation.md",
                                 "tutorials/bifurcation_diagrams.md",
                                 "tutorials/parameter_estimation.md",
-                                "tutorials/smoluchowski_coagulation_equation.md"
+            
+            "Example Networks" => Any[
+                                "example_networks/basic_CRN_examples.md",
+                                "example_networks/smoluchowski_coagulation_equation.md"
                             ],
             "FAQs" => "faqs.md",
             "API" => "api/catalyst_api.md"
