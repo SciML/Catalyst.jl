d6197a6e79d8b4afc3105221a458953a1ac57ac4
Torkel <torkel.loman@gmail.com>
2023-02-03 12:11:29 -0500

    Section titles.

--- docs/pages.jl
+++ docs/pages.jl
@@ -1,15 +1,24 @@
 pages = Any["Home" => "index.md",
-            "Catalyst for new Julia users" => Any["catalyst_for_new_julia_users/intro_to_catalyst.md"],
-            "Tutorials" => Any["tutorials/using_catalyst.md",
-                               "tutorials/dsl.md",
-                               "tutorials/reaction_systems.md",
-                               "tutorials/basic_examples.md",
-                               "tutorials/compositional_modeling.md",
-                               "tutorials/symbolic_stoich.md",
-                               "tutorials/reaction_network_representation.md",
-                               "tutorials/homotopy_continuation_tutorial.md",
-                               "tutorials/bifurcation_diagram.md",
-                               "tutorials/parameter_estimation.md",
-                               "tutorials/generating_reactions_programmatically.md"],
+            "Introduction to Catalyst" => Any[
+                                "intro_to_catalyst/catalyst_for_new_julia_users.md",
+                                "intro_to_catalyst/using_catalyst.md"
+                            ],
+            "Catalyst Functionality" => Any[
+                                "catalyst_functionality/dsl.md",
+                                "catalyst_functionality/reaction_systems.md",
+                                "catalyst_functionality/basic_examples.md",
+                                "catalyst_functionality/compositional_modeling.md",
+                                "catalyst_functionality/symbolic_stoich.md"
+                            ],
+            "Tutorials" => Any[
+                                "tutorials/reaction_network_representation.md",
+                                "tutorials/homotopy_continuation_tutorial.md",
+                                "tutorials/bifurcation_diagram.md",
+                                "tutorials/parameter_estimation.md",
+                                "tutorials/generating_reactions_programmatically.md"
+                            ],
             "FAQs" => "faqs.md",
-            "API" => Any["api/catalyst_api.md"]]
+            "API" => Any[
+                            "api/catalyst_api.md"
+                        ]
+            ]
