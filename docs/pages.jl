eb27a04088de1189daa225cab6f6b75010876b48
Torkel <torkel.loman@gmail.com>
2023-02-06 14:10:47 -0500

    format

--- docs/pages.jl
+++ docs/pages.jl
@@ -1,25 +1,15 @@
-pages = Any[
-            "Home" => "index.md",
-            "Introduction to Catalyst" => Any[
-                                "intro_to_catalyst/catalyst_for_new_julia_users.md",
-                                "intro_to_catalyst/using_catalyst.md"
-                            ],
-            "Catalyst Functionality" => Any[
-                                "catalyst_functionality/dsl_description.md",
-                                "catalyst_functionality/programmatic_CRN_construction.md",
-                                "catalyst_functionality/compositional_modeling.md",
-                                "catalyst_functionality/parametric_stoichiometry.md",
-                                "catalyst_functionality/network_analysis.md"
-                            ],
-            "Catalyst Applications" => Any[
-                                "tutorials/homotopy_continuation.md",
-                                "tutorials/bifurcation_diagrams.md",
-                                "tutorials/parameter_estimation.md"
-                            ],
-            "Example Networks" => Any[
-                                "example_networks/basic_CRN_examples.md",
-                                "example_networks/smoluchowski_coagulation_equation.md"
-                            ],
+pages = Any["Home" => "index.md",
+            "Introduction to Catalyst" => Any["intro_to_catalyst/catalyst_for_new_julia_users.md",
+                                              "intro_to_catalyst/using_catalyst.md"],
+            "Catalyst Functionality" => Any["catalyst_functionality/dsl_description.md",
+                                            "catalyst_functionality/programmatic_CRN_construction.md",
+                                            "catalyst_functionality/compositional_modeling.md",
+                                            "catalyst_functionality/parametric_stoichiometry.md",
+                                            "catalyst_functionality/network_analysis.md"],
+            "Catalyst Applications" => Any["tutorials/homotopy_continuation.md",
+                                           "tutorials/bifurcation_diagrams.md",
+                                           "tutorials/parameter_estimation.md"],
+            "Example Networks" => Any["example_networks/basic_CRN_examples.md",
+                                      "example_networks/smoluchowski_coagulation_equation.md"],
             "FAQs" => "faqs.md",
-            "API" => "api/catalyst_api.md"
-            ]
+            "API" => "api/catalyst_api.md"]
