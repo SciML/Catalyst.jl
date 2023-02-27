a93e6416c24f769bbe1b488a716a3cc2e8c6b3c7
Torkel <torkel.loman@gmail.com>
2023-02-27 11:31:29 -0500

    plotting

--- test/visualization/plotting.jl
+++ test/visualization/plotting.jl
@@ -2,7 +2,7 @@
 ### Fetch required packages and reaction networks ###
 using Catalyst, JumpProcesses, OrdinaryDiffEq, Plots, Random, Test
 using ModelingToolkit: get_states, get_ps
-include("test_networks.jl")
+include("../test_networks.jl")
 
 using StableRNGs
 rng = StableRNG(12345)
