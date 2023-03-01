73e2db666dada336f253fd74d5c7f5c1537be6c3
Torkel <torkel.loman@gmail.com>
2023-03-01 17:43:55 -0500

    modify

--- test/visualization/graphs.jl
+++ test/visualization/graphs.jl
@@ -2,22 +2,22 @@ using Catalyst, Graphviz_jll
 
 ### Basic Tests ###
 #let
-    rn = @reaction_network begin
-        α, S + I --> 2I
-        β, I --> R
-        S^2, R --> 0
-    end
+rn = @reaction_network begin
+    α, S + I --> 2I
+    β, I --> R
+    S^2, R --> 0
+end
 
-    # Check can make a graph.
-    gr = Graph(rn)
+# Check can make a graph.
+gr = Graph(rn)
 
-    # Check can save a graph.
-    fname = Base.Filesystem.tempname()
-    savegraph(gr, "$fname.svg", "svg")
+# Check can save a graph.
+fname = Base.Filesystem.tempname()
+savegraph(gr, "$fname.svg", "svg")
 
-    rcgr = complexgraph(rn)
-    fname = Base.Filesystem.tempname()
-    savegraph(rcgr, "$fname.svg", "svg")
+rcgr = complexgraph(rn)
+fname = Base.Filesystem.tempname()
+savegraph(rcgr, "$fname.svg", "svg")
 #end
 
 # these are broken in the jll, see
--- test/visualization/plotting.jl
+++ test/visualization/plotting.jl
@@ -15,21 +15,21 @@ include("../test_networks.jl")
 
 # Tests the plot() function on a few basic simulation solutions, checks that there are no errors.
 #let
-    plotting_test_networks = [
-        reaction_networks_standard[6],
-        reaction_networks_constraint[6],
-        reaction_networks_real[3],
-    ]
-    for network in plotting_test_networks, factor in [1e-1, 1e0, 1e1]
-        u0 = factor * rand(rng, length(get_states(network)))
-        p = factor * rand(rng, length(get_ps(network)))
-        sol = solve(ODEProblem(network, u0, (0.0, 1), p), Rosenbrock23())
-        plot(sol)
+plotting_test_networks = [
+    reaction_networks_standard[6],
+    reaction_networks_constraint[6],
+    reaction_networks_real[3],
+]
+for network in plotting_test_networks, factor in [1e-1, 1e0, 1e1]
+    u0 = factor * rand(rng, length(get_states(network)))
+    p = factor * rand(rng, length(get_ps(network)))
+    sol = solve(ODEProblem(network, u0, (0.0, 1), p), Rosenbrock23())
+    plot(sol)
 
-        u0 = rand(rng, 1:Int64(factor * 100), length(get_states(network)))
-        sol = solve(JumpProblem(network, DiscreteProblem(network, u0, (0.0, 1), p),
-                                Direct()),
-                    SSAStepper())
-        plot(sol)
-    end
+    u0 = rand(rng, 1:Int64(factor * 100), length(get_states(network)))
+    sol = solve(JumpProblem(network, DiscreteProblem(network, u0, (0.0, 1), p),
+                            Direct()),
+                SSAStepper())
+    plot(sol)
+end
 #end
