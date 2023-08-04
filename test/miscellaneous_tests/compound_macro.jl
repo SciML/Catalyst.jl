e9ad3e7da78d07e144feb47e231b5f17621236ad
Lalit Chauhan <lalitchauhanlucky@gmail.com>
2023-08-04 13:31:23 +0530

    Update tests

--- test/miscellaneous_tests/compound_macro.jl
+++ test/miscellaneous_tests/compound_macro.jl
@@ -14,7 +14,7 @@ let
 
     @test isequal([C, H, O], components(C6H12O2))
     @test isequal([6, 12, 2], coefficients(C6H12O2))
-    @test isequal([C => 6, H => 12, O => 2], Catalyst.component_coefficients(C6H12O2))
+    @test isequal([C => 6, H => 12, O => 2], Catalyst.component_coefficients(C6H12O2)) 
     @test all(!iscompound(i) for i in components(C6H12O2))
 end
 
@@ -74,6 +74,9 @@ let
     @compound C6H12O2_1(t) 6s 12H 2O
     @compound C6H12O2_2(t) 6C 12H 2O
 
+    @test iscompound(C6H12O2_1)
+    @test iscompound(C6H12O2_2)
+
     @test isequal(components(C6H12O2_1), components(C6H12O2_2))
     @test isequal(coefficients(C6H12O2_1), coefficients(C6H12O2_2))
 end
@@ -93,3 +96,83 @@ let
     @test isequal(coefficients(C10H12)[1], 2)
 end
 
+let
+    @variables t
+    @species H(t)
+
+    alpha = 2
+    @compound H2_1(t) alpha*H
+    @compound H2_2(t) 2H
+
+    @test iscompound(H2_1)
+    @test iscompound(H2_2)
+
+    @test isequal(components(H2_1),components(H2_2))
+    @test isequal(coefficients(H2_1),coefficients(H2_2))
+end
+
+let
+    @variables t
+    @parameters alpha = 2
+    @species H(t)
+
+    @compound H2_1(t) alpha*H
+    @compound H2_2(t) 2H
+
+    @test iscompound(H2_1)
+    @test iscompound(H2_2)
+
+    @test isequal(components(H2_1),components(H2_2))
+    @test isequal(coefficients(H2_1), @parameters alpha = 2)
+end
+
+let 
+    @variables t
+    @species A(t)
+    B = A
+    @compound A2(t) 2A
+    @compound B2(t) 2B
+
+    @test iscompound(A2)
+    @test iscompound(B2)
+
+    @test isequal(components(A2),components(B2))
+    @test isequal(coefficients(A2), coefficients(B2))
+end
+
+#Check that balancing works.
+let 
+    @variables t
+    @parameters k
+    @species H(t) O(t)
+    @compound H2(t) 2H
+    @compound O2(t) 2O
+    @compound H2O(t) 2H 1O
+
+    rx = Reaction(k,[H2,O2],[H2O])
+
+    @test isequal(create_matrix(rx),[2 0 -2; 0 2 -1; 0 0 1;])
+    @test isequal(get_stoich(rx),[2, 1, 2])
+
+    balanced_rx = Reaction(k,[H2,O2],[H2O],[2,1],[2])
+    @test isequal(balanced_rx, balance(rx))
+
+end
+
+let 
+    @variables t
+    @parameters k
+    @species C(t) H(t) O(t) 
+    @compound O2(t) 2O
+    @compound CO2(t) 1C 2O
+    @compound H2O(t) 2H 1O
+    @compound C6H12O6(t) 6C 12H 6O
+
+    rx = Reaction(k,[CO2,H2O],[C6H12O6,O2])
+
+    @test isequal(create_matrix(rx),[ 1 0 -6 0; 2 1 -6 -2; 0 2 -12 0; 0 0 0 1;])
+    @test isequal(get_stoich(rx),[6, 6, 1, 6])
+
+    balanced_rx = Reaction(k,[CO2,H2O],[C6H12O6,O2],[6, 6], [1,6])
+    @test isequal(balanced_rx, balance(rx))
+end
