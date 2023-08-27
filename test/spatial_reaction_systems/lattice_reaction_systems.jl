3bb908ed63d86fdcb54c35382447c41ce11f028d
Torkel <torkel.loman@gmail.com>
2023-08-27 09:44:29 -0400

    test update

--- test/spatial_reaction_systems/lattice_reaction_systems.jl
+++ test/spatial_reaction_systems/lattice_reaction_systems.jl
@@ -675,7 +675,7 @@ let
 end
 
 # Tests that the correct hopping rates and initial conditions are generated.
-# Hoppping rates should be on the form D_{s,i,j}.
+# In this base case, hoppping rates should be on the form D_{s,i,j}.
 let
     # Prepares the system.
     lrs = LatticeReactionSystem(SIR_system, SIR_srs_2, small_2d_grid)
@@ -687,7 +687,7 @@ let
     u0_4 = [1.0, fill(2., nv(small_2d_grid)), 3.0]
     u0_5 = permutedims(hcat(fill(1., nv(small_2d_grid)), fill(2., nv(small_2d_grid)), fill(3., nv(small_2d_grid))))
 
-    # Prepare various (comaprtment) parameter input types.
+    # Prepare various (compartment) parameter input types.
     pC_1 = [:β => 0.2, :α => 0.1]
     pC_2 = [:β => fill(0.2, nv(small_2d_grid)), :α => 1.0]
     pC_3 = [0.1, 0.2]
@@ -701,12 +701,12 @@ let
     pD_4 = [fill(0.01, ne(small_2d_grid)), 0.02, 0.03]
     pD_5 = permutedims(hcat(fill(0.01, ne(small_2d_grid)), fill(0.02, ne(small_2d_grid)), fill(0.03, ne(small_2d_grid))))
 
-    # Checks hopping rates and u0 correct.
+    # Checks hopping rates and u0 are correct.
     true_u0 = [fill(1.0, 1, 25); fill(2.0, 1, 25); fill(3.0, 1, 25)]
     true_hopping_rates = cumsum.([fill(dval, length(v)) for dval in [0.01,0.02,0.03], v in small_2d_grid.fadjlist])
     for u0 in [u0_1, u0_2, u0_3, u0_4, u0_5]
         # Provides parameters as a tupple.
-        for pC in [pC_1, pC_3], pD in [pD_1, pD_2, pC_3, pD_4, pD_5]
+        for pC in [pC_1, pC_3], pD in [pD_1, pD_2, pD_3, pD_4, pD_5]
             dprob = DiscreteProblem(lrs, u0, (0.0, 100.0), (pC,pD))
             jprob = JumpProblem(lrs, dprob, NSM())
             @test jprob.prob.u0 == true_u0
@@ -717,6 +717,198 @@ let
             dprob = DiscreteProblem(lrs, u0, (0.0, 100.0), [pD; pC])
             jprob = JumpProblem(lrs, dprob, NSM())
             @test jprob.prob.u0 == true_u0
+            @test jprob.discrete_jump_aggregation.hop_rates.hop_const_cumulative_sums == true_hopping_rates
+        end
+    end
+end
+
+# Currently not in use, but will be added as more cases are enabled.
+if false
+    # Tests hopping rates of the form D_{s,i,j}
+    let
+        # Prepares the system.
+        lrs = LatticeReactionSystem(SIR_system, SIR_srs_2, small_2d_grid)
+        u0 = [:I => 2.0, :S => 1.0, :R => 3.0]
+        pC = [:β => 0.2, :α => 0.1]
+
+        # Prepare various (diffusion) parameter input types.
+        pD_1 = [:dI => 0.02, :dS => 0.01, :dR => 0.03]
+        pD_2 = [:dI => 0.02, :dS => fill(0.01, ne(lrs.lattice)), :dR => 0.03]
+        pD_3 = [0.01, 0.02, 0.03]
+        pD_4 = [fill(0.01, ne(lrs.lattice)), 0.02, 0.03]
+        pD_5 = permutedims(hcat(fill(0.01, ne(lrs.lattice)), fill(0.02, ne(lrs.lattice)), fill(0.03, ne(lrs.lattice))))
+
+        # Checks hopping rates are correct.
+        true_hopping_rates = []
+        # Provides parameters as a tupple.
+        for pD in [pD_1, pD_2, pD_3, pD_4, pD_5]
+            dprob = DiscreteProblem(lrs, u0, (0.0, 100.0), (pC,pD))
+            jprob = JumpProblem(lrs, dprob, NSM())
+            @test jprob.discrete_jump_aggregation.hop_rates.hop_const_cumulative_sums == true_hopping_rates
+        end
+        # Provides parameters as a combined vector.
+        for pD in [pD_1, pD_2]
+            dprob = DiscreteProblem(lrs, u0, (0.0, 100.0), [pD; pC])
+            jprob = JumpProblem(lrs, dprob, NSM())
+            @test jprob.discrete_jump_aggregation.hop_rates.hop_const_cumulative_sums == true_hopping_rates
+        end
+    end
+
+    # Tests hopping rates of the form D_{s}
+    let
+        # Prepares the system.
+        lrs = LatticeReactionSystem(SIR_system, SIR_srs_2, small_2d_grid)
+        u0 = [:I => 2.0, :S => 1.0, :R => 3.0]
+        pC = [:β => 0.2, :α => 0.1]
+
+        # Prepare various (diffusion) parameter input types.
+        pD_1 = [:dI => 0.02, :dS => 0.01, :dR => 0.03]
+        pD_3 = [0.01, 0.02, 0.03]
+
+        # Checks hopping rates are correct.
+        true_hopping_rates = []
+        # Provides parameters as a tupple.
+        for pD in [pD_1, pD_3]
+            dprob = DiscreteProblem(lrs, u0, (0.0, 100.0), (pC,pD))
+            jprob = JumpProblem(lrs, dprob, NSM())
+            @test jprob.discrete_jump_aggregation.hop_rates.hop_const_cumulative_sums == true_hopping_rates
+        end
+        # Provides parameters as a combined vector.
+        for pD in [pD_1]
+            dprob = DiscreteProblem(lrs, u0, (0.0, 100.0), [pD; pC])
+            jprob = JumpProblem(lrs, dprob, NSM())
+            @test jprob.discrete_jump_aggregation.hop_rates.hop_const_cumulative_sums == true_hopping_rates
+        end
+    end
+
+    # Tests hopping rates of the form D_{s,i}
+    let
+        # Prepares special system and diffusion reactions (depending on compartments).
+        SIR_system_special = @reaction_network begin
+            @parameters comp_dS comp_dI
+            α, S + I --> 2I
+            β, I --> R
+        end
+        @unpack comp_dS, comp_dI = SIR_system_special
+        SIR_srs_special = [DiffusionReaction(comp_dS, :S), DiffusionReaction(comp_dI, :I)]
+
+        # Prepares the system.
+        lrs = LatticeReactionSystem(SIR_system_special, SIR_srs_special, small_2d_grid)
+        u0 = [:I => 2.0, :S => 1.0, :R => 3.0]
+        pD = []
+
+        # Prepare various (compartment) parameter input types.
+        pC_1 = [:β => 0.2, :α => 0.1, :comp_dI => 0.02, :comp_dS => 0.01]
+        pC_2 = [:β => fill(0.2, nv(small_2d_grid)), :α => 1.0, :comp_dI => fill(0.02, nv(small_2d_grid)), :comp_dS => 0.01]
+        pC_3 = [:β => 0.2, :α => 1.0, :comp_dI => fill(0.02, nv(small_2d_grid)), :comp_dS => 0.01]
+        pC_4 = [0.1, 0.2, 0.01, 0.02]
+        pC_5 = [0.1, 0.2, 0.01, fill(0.02, nv(small_2d_grid))]
+        pC_6 = [0.1, fill(0.2, nv(small_2d_grid)), 0.01, fill(0.02, nv(small_2d_grid))]
+        pC_7 = permutedims(hcat(fill(0.1, nv(small_2d_grid)), fill(0.2, nv(small_2d_grid)), fill(0.01, nv(small_2d_grid)), fill(0.02, nv(small_2d_grid))))
+
+        # Checks hopping rates are correct.
+        true_hopping_rates = []
+        # Provides parameters as a tupple.
+        for pC in [pC_1, pC_2, pC_4, pC_5]
+            dprob = DiscreteProblem(lrs, u0, (0.0, 100.0), (pC,pD))
+            jprob = JumpProblem(lrs, dprob, NSM())
+            @test jprob.discrete_jump_aggregation.hop_rates.hop_const_cumulative_sums == true_hopping_rates
+        end
+        # Provides parameters as a combined vector.
+        for pC in [pC_1, pC_2]
+            dprob = DiscreteProblem(lrs, u0, (0.0, 100.0), [pD; pC])
+            jprob = JumpProblem(lrs, dprob, NSM())
+            @test jprob.discrete_jump_aggregation.hop_rates.hop_const_cumulative_sums == true_hopping_rates
+        end
+    end
+
+    # Tests hopping rates of the form D_{s}*L_{i,j}
+    # Since dS and dI are constant across the system, these can both be diffusionparameter or not (bot hcases tested).
+    let
+        SIR_system_special = @reaction_network begin
+            @parameters dS [diffusionparameter = true] dI connection_d [diffusionparameter = true] 
+            α, S + I --> 2I
+            β, I --> R
+        end
+        @unpack dS, dI = SIR_system_special
+        SIR_srs_special = [DiffusionReaction(dS*connection_d, :S), DiffusionReaction(dI*connection_d, :I)]
+
+        # Prepares the system.
+        lrs = LatticeReactionSystem(SIR_system_special, SIR_srs_special, small_2d_grid)
+        u0 = [:I => 2.0, :S => 1.0, :R => 3.0]
+        pC = [:β => 0.2, :α => 0.1, :dI => 0.01]
+
+        # Prepare various (compartment) parameter input types.
+        pC_1 = [:β => 0.2, :α => 0.1, ]
+        pC_2 = [:β => fill(0.2, nv(small_2d_grid)), :α => 1.0]
+        pC_3 = [0.1, 0.2]
+        pC_4 = [0.1, fill(0.2, nv(small_2d_grid))]
+        pC_5 = permutedims(hcat(fill(0.1, nv(small_2d_grid)), fill(0.2, nv(small_2d_grid))))
+
+        # Prepare various (diffusion) parameter input types.
+        pD_1 = [:connection_d => 2.0, :dS => 0.005]
+        pD_2 = [:connection_d => fill(0.2, ne(small_2d_grid)), :dS => 0.005]
+        pD_3 = [0.005, 2.0]
+        pD_4 = [0.005, fill(0.2, ne(small_2d_grid))]
+
+        # Checks hopping rates are correct.
+        true_hopping_rates = []
+        # Provides parameters as a tupple.
+        for pD in [pD_1, pD_2, pD_3, pD_4]
+            dprob = DiscreteProblem(lrs, u0, (0.0, 100.0), (pC,pD))
+            jprob = JumpProblem(lrs, dprob, NSM())
+            @test jprob.discrete_jump_aggregation.hop_rates.hop_const_cumulative_sums == true_hopping_rates
+        end
+        # Provides parameters as a combined vector.
+        for pD in [pD_1, pD_2]
+            dprob = DiscreteProblem(lrs, u0, (0.0, 100.0), [pD; pC])
+            jprob = JumpProblem(lrs, dprob, NSM())
+            @test jprob.discrete_jump_aggregation.hop_rates.hop_const_cumulative_sums == true_hopping_rates
+        end
+    end
+
+    # Tests hopping rates of the form D_{s,i}*L_{i,j}
+    let
+        SIR_system_special = @reaction_network begin
+            @parameters comp_dS comp_dI connection_d [diffusionparameter = true] 
+            α, S + I --> 2I
+            β, I --> R
+        end
+        @unpack comp_dS, comp_dI connection_d = SIR_system_special
+        SIR_srs_special = [DiffusionReaction(comp_dS*connection_d, :S), DiffusionReaction(comp_dI*connection_d, :I)]
+
+        # Prepares the system.
+        lrs = LatticeReactionSystem(SIR_system_special, SIR_srs_special, small_2d_grid)
+        u0 = [:I => 2.0, :S => 1.0, :R => 3.0]
+
+        # Prepare various (compartment) parameter input types.
+        pC_1 = [:β => 0.2, :α => 0.1, :comp_dI => 0.02, :comp_dS => 0.01]
+        pC_2 = [:β => fill(0.2, nv(small_2d_grid)), :α => 1.0, :comp_dI => fill(0.02, nv(small_2d_grid)), :comp_dS => 0.01]
+        pC_3 = [:β => 0.2, :α => 1.0, :comp_dI => fill(0.02, nv(small_2d_grid)), :comp_dS => 0.01]
+        pC_4 = [0.1, 0.2, 0.01, 0.02]
+        pC_5 = [0.1, 0.2, 0.01, fill(0.02, nv(small_2d_grid))]
+        pC_6 = [0.1, fill(0.2, nv(small_2d_grid)), 0.01, fill(0.02, nv(small_2d_grid))]
+        pC_7 = permutedims(hcat(fill(0.1, nv(small_2d_grid)), fill(0.2, nv(small_2d_grid)), fill(0.01, nv(small_2d_grid)), fill(0.02, nv(small_2d_grid))))
+
+        # Prepare various (diffusion) parameter input types.
+        pD_1 = [:connection_d => 2.0]
+        pD_2 = [:connection_d => fill(0.2, ne(small_2d_grid))]
+        pD_3 = [2.0]
+        pD_4 = [fill(0.2, ne(small_2d_grid))]
+        pD_5 = [fill(0.2, 1, ne(small_2d_grid))]
+
+        # Checks hopping rates are correct.
+        true_hopping_rates = []
+        # Provides parameters as a tupple.
+        for pC in [pC_1, pC_2, pC_4, pC_5], pD in [pD_1, pD_2, pD_3, pD_4, pD_5]
+            dprob = DiscreteProblem(lrs, u0, (0.0, 100.0), (pC,pD))
+            jprob = JumpProblem(lrs, dprob, NSM())
+            @test jprob.discrete_jump_aggregation.hop_rates.hop_const_cumulative_sums == true_hopping_rates
+        end
+        # Provides parameters as a combined vector.
+        for pC in [pC_1, pC_2], pD in [pD_1, pD_2]
+            dprob = DiscreteProblem(lrs, u0, (0.0, 100.0), [pD; pC])
+            jprob = JumpProblem(lrs, dprob, NSM())
             @test jprob.discrete_jump_aggregation.hop_rates.hop_const_cumulative_sums == true_hopping_rates
         end
     end
