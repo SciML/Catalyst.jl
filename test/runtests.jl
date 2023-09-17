591a9f58f3fd81317245446a0215dc3772be899c
Torkel <torkel.loman@gmail.com>
2023-09-17 14:24:27 -0400

    Test fix

--- test/runtests.jl
+++ test/runtests.jl
@@ -45,6 +45,7 @@ using SafeTestsets
     @time @safetestset "PDE Systems Simulations" begin include("spatial_reaction_systems/simulate_PDEs.jl") end
     @time @safetestset "Lattice Reaction Systems" begin include("spatial_reaction_systems/lattice_reaction_systems.jl") end
     @time @safetestset "ODE Lattice Systems Simulations" begin include("spatial_reaction_systems/lattice_reaction_systems_ODEs.jl") end
+    @time @safetestset "Jump Lattice Systems Simulations" begin include("spatial_reaction_systems/lattice_reaction_systems_jumps.jl") end
 
     ### Tests network visualization. ###
     @time @safetestset "Latexify" begin include("visualization/latexify.jl") end
