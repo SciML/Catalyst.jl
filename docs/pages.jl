d89ec94def8cd1c0abd115746d5e324c23431b37
Torkel <torkel.loman@gmail.com>
2023-10-04 16:13:42 -0400

    update

--- docs/pages.jl
+++ docs/pages.jl
@@ -1,12 +1,15 @@
 pages = Any["Home" => "index.md",
             "Introduction to Catalyst" => Any["introduction_to_catalyst/catalyst_for_new_julia_users.md",
-                                              "introduction_to_catalyst/introduction_to_catalyst.md"],
+                                              "introduction_to_catalyst/introduction_to_catalyst.md" ],
             "Catalyst Functionality" => Any["catalyst_functionality/dsl_description.md",
                                             "catalyst_functionality/programmatic_CRN_construction.md",
                                             "catalyst_functionality/compositional_modeling.md",
                                             "catalyst_functionality/constraint_equations.md",
                                             "catalyst_functionality/parametric_stoichiometry.md",
-                                            "catalyst_functionality/network_analysis.md"],
+                                            "catalyst_functionality/network_analysis.md",
+                                            "Model creation examples" => Any["example_networks/basic_CRN_examples.md",
+                                                                             "example_networks/hodgkin_huxley_equation.md",
+                                                                             "example_networks/smoluchowski_coagulation_equation.md"]],
             "Catalyst Applications" => Any["catalyst_applications/simulation_structure_interfacing.md",
                                            "catalyst_applications/advanced_simulations.md",
                                            "catalyst_applications/homotopy_continuation.md",
@@ -16,9 +19,12 @@ pages = Any["Home" => "index.md",
                                       "petab_ode_param_fitting.md"],
 =======
             "Inverse Problems" => Any["inverse_problems/parameter_estimation.md"],
+<<<<<<< refs/remotes/origin/master
 >>>>>>> Inverse Problems
             "Example Networks" => Any["example_networks/basic_CRN_examples.md",
                                       "example_networks/hodgkin_huxley_equation.md",
                                       "example_networks/smoluchowski_coagulation_equation.md"],
+=======
+>>>>>>> update
             "FAQs" => "faqs.md",
             "API" => "api/catalyst_api.md"]
