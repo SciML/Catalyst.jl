131ebf3153a0c76d87d333b259c2852c536e42e1
Torkel <torkel.loman@gmail.com>
2023-10-27 13:09:36 -0400

    save

--- ext/CatalystBifurcationKitExtension/bifurcation_kit_extension.jl
+++ ext/CatalystBifurcationKitExtension/bifurcation_kit_extension.jl
@@ -1,9 +1,16 @@
 ### Dispatch for BifurcationKit BifurcationProblems ###
 
+# Creates a BifurcationProblem, using a ReactionSystem as an input.
 function BK.BifurcationProblem(rs::ReactionSystem, u0_bif, ps, bif_par, args...; plot_var=nothing, record_from_solution=BK.record_sol_default, jac=true, u0=[], kwargs...)
+    # Converts symbols to symbolics.
+    (bif_par isa Symbol) && (bif_par = rs.var_to_name[bif_par])
+    (plot_var isa Symbol) && (plot_var = rs.var_to_name[plot_var])
 
+    # Creates NonlinearSystem.
+    conservationlaw_errorcheck(rs, vcat(ps, u0))
+    nsys = covnert(NonlinearSystem, rs; defaults=u0)
 
-
-    return BK.BifurcationProblem(nsys,)
+    # Makes BifurcationProblem.
+    return BK.BifurcationProblem(nsys, u0_bif, ps, bif_par, args...; plot_var=plot_var, record_from_solution=record_from_solution, jac=jac, kwargs...)
 end
 
--- test/extensions/bifurcation_kit.jl
+++ test/extensions/bifurcation_kit.jl
@@ -3,6 +3,14 @@ using Bifurcationkit, Catalyst, Test
 
 ### Run Tests ###
 
+# Brusselator, tests that a Hopf bifurcation is found.
 let 
     
 end
+
+# Bistable switch with deflation, tests that bifurcation diagrams works while multistability exists for initial point.
+let 
+    
+end
+
+# Three-state system, tests that bifurcation diagrams works for systems with conserved quantities.
