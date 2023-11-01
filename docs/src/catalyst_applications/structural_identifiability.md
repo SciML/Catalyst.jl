1da978eeeb18ca3ca0afbdec3302ddafa6bd561d
Torkel <torkel.loman@gmail.com>
2023-11-01 18:41:23 -0400

    update

--- docs/src/catalyst_applications/structural_identifiability.md
+++ docs/src/catalyst_applications/structural_identifiability.md
@@ -1,2 +1,35 @@
-# [StructuralIdentifiability Analysis](@id structural_identifiability)
-During parameter fitting, parameter values are inferred from data. Identifiability is a concept describing to what extent the identification of parameter values for a certain model is actually feasible. Ideally, parameter fitting should always be accompanied with an identifiability analysis of the problem. Identifiability can be divided into *structural* and *practical* identifiability. Structural identifiability considers only  the system and what quantities we can measure to determine which quantities can be identified. Practical identifiability instead considers teh available data, and determines what system quantities can be infeed from it. Generally, in the hypothetical case of an infinite amount of without noise, practical identifiability becomes identical to structural identifiability. 
+# [Structural Identifiability Analysis](@id structural_identifiability)
+During parameter fitting, parameter values are inferred from data. Identifiability is a concept describing to what extent the identification of parameter values for a certain model is actually feasible. Ideally, parameter fitting should always be accompanied with an identifiability analysis of the problem. Identifiability can be divided into *structural* and *practical* identifiability[^1]. Structural identifiability considers only  the system and what quantities we can measure to determine which quantities can be identified. Practical identifiability instead considers the available data, and determines what system quantities can be infeed from it. Generally, in the hypothetical case of an infinite amount of without noise, practical identifiability becomes identical to structural identifiability. Generally, structural identifiability is assessed before parameters are fitted, while practical identifiability is assessed afterwards.
+
+Structural identifiability can be illustrated in the following example network:
+${dx \over dt} = p1*p2*x(t)$
+where, however much data I have on *x*, it is impossible to determine the values of *p1* and *p2* (these are non-identifiable).
+
+Catalyst contains a special extension for carrying out structural identifiability analysis using the [StructuralIdentifiability.jl](https://github.com/SciML/StructuralIdentifiability.jl) package. This enables StructuralIdentifiability's `assess_identifiability` and `assess_local_identifiability` functions to be called directly on Catalyst `ReactionSystems`. How to use these functions are described in the following tutorial, with [StructuralIdentifiability providing a more extensive documentation](https://docs.sciml.ai/StructuralIdentifiability/stable/). If you use this in your research, please [cite the StructuralIdentifiability.jl](@ref structural_identifiability_citation) and [Catalyst.jl](@ref catalyst_citation) publications.
+
+## Global sensitivity analysis
+
+## Local sensitivity analysis
+
+## Creating StructuralIdentifiability compatible ODE models from Catalyst `ReactionSystem`s
+
+
+---
+## [Citation](@id structural_identifiability_citation)
+If you use this functionality in your research, please cite the following paper to support the authors of the StructuralIdentifiability package:
+```
+@article{structidjl,
+  author  = {Dong, R. and Goodbrake, C. and Harrington, H. and Pogudin G.},
+  title   = {Differential Elimination for Dynamical Models via Projections with Applications to Structural Identifiability},
+  journal = {SIAM Journal on Applied Algebra and Geometry},
+  url     = {https://doi.org/10.1137/22M1469067},
+  year    = {2023}
+  volume  = {7},
+  number  = {1},
+  pages   = {194-235}
+}
+```
+
+---
+## References
+[^1]: [Guillaume H.A. Joseph et al., *Introductory overview of identifiability analysis: A guide to evaluating whether you have the right type of data for your modeling purpose*, Environmental Modelling & Software (2019).](https://www.sciencedirect.com/science/article/pii/S1364815218307278)
--- ext/CatalystStructuralIdentifiabilityExtension/structural_identifiability_extension.jl
+++ ext/CatalystStructuralIdentifiabilityExtension/structural_identifiability_extension.jl
@@ -1,24 +1,50 @@
-### Structural Identifiability Analysis ###
-
-# Local identifiability.
-function StructuralIdentifiability.assess_local_identifiability(rs::ReactionSystem, args...; measured_quantities=observed(rs), known_p = [], kwargs...)
-    return StructuralIdentifiability.assess_local_identifiability(convert(ODESystem, rs), args...; measured_quantities=get_measured_quantities(rs, measured_quantities, known_p), kwargs...)
-end
-
-# Global identifiability.
-function StructuralIdentifiability.assess_identifiability(rs::ReactionSystem, args...; measured_quantities=observed(rs), known_p = [], kwargs...)
-    return StructuralIdentifiability.assess_identifiability(convert(ODESystem, rs), args...; measured_quantities=get_measured_quantities(rs, measured_quantities, known_p), kwargs...)
-end
+### Structural Identifiability ODE Creation ###
 
 # For a reaction system, list of measured quantities and known parameters, generate a StructuralIdentifiability compatible ODE. 
+"""
+    si_ode(rs::ReactionSystem; measured_quantities=observed(rs), known_p = Num[], ignore_no_measured_warn=false)
+
+Creates a ODE system of the form used within the StructuralIdentifiability.jl package. The output system is compatible with all StructuralIdentifiability functions. 
+
+Arguments:
+- `rs::ReactionSystem`; The reaction system we wish to convert to an ODE.
+- `measured_quantities=observed(rs)`: The quantities of the system we can measure. May either be equations (e.g. `x1 + x2`), or single species (e.g. `:x`). Defaults to the systems observables.
+- `known_p = Num[]`: List of parameters which values are known. 
+- `ignore_no_measured_warn=false`: If set to `true`, no warning is provided when the `measured_quantities` vector is empty. 
+
+Example:
+```julia
+rs = @reaction_network begin
+    (p,d), 0 <--> X
+end
+si_ode(rs; measured_quantities = [:X], known_p = [:p])
+```
+"""
+function make_si_ode(rs::ReactionSystem; measured_quantities=observed(rs), known_p = Num[], ignore_no_measured_warn=false)
+    ignore_no_meassured_warn || isempty(measured_quantities) && @warn "No measured quantity provided to the `measured_quantities` argument, any further identifiability analysis will likely fail."
+    known_quantities = make_measured_quantities(rs, measured_quantities, known_p)
+    return preprocess_ode(convert(ODESystem, rs), known_quantities)
+end
 
 # For input measured quantities, if this is not a vector of equations, convert it to a proper form.
-get_measured_quantities(rs::ReactionSystem, measured_quantities::Vector{Equation}) = measured_quantities # If form is already a vector of equations.
-function get_measured_quantities(rs::ReactionSystem, measured_quantities::Vector{T}, known_p::Vector{S}) where {T,S}
+make_measured_quantities(rs::ReactionSystem, measured_quantities::Vector{Equation}) = measured_quantities # If form is already a vector of equations.
+function make_measured_quantities(rs::ReactionSystem, measured_quantities::Vector{T}, known_p::Vector{S}) where {T,S}
     (measured_quantities isa Vector{Symbol}) && (measured_quantities = [Catalyst._symbol_to_var(rs, sym) for sym in measured_quantities])
     (known_p isa Vector{Symbol}) && (known_p = [Catalyst._symbol_to_var(rs, sym) for sym in known_p])
     @variables t ___internal_observables[1:length(measured_quantities)](t)
     measured_quantities = [eq[1] ~ eq[2] for eq in zip(___internal_observables, measured_quantities)] 
-    return [measured_quantities; known_p]
+    return [Num.(measured_quantities); known_p]
+end
+
+### Structural Identifiability Wrappers ###
+
+# Local identifiability.
+function StructuralIdentifiability.assess_local_identifiability(rs::ReactionSystem, args...; measured_quantities=observed(rs), known_p = Num[], kwargs...)
+    return StructuralIdentifiability.assess_local_identifiability(make_si_ode(rs, measured_quantities, known_p), args...; kwargs...)
+end
+
+# Global identifiability.
+function StructuralIdentifiability.assess_identifiability(rs::ReactionSystem, args...; measured_quantities=observed(rs), known_p = Num[], kwargs...)
+    return StructuralIdentifiability.assess_identifiability(make_si_ode(rs, measured_quantities, known_p), args...; kwargs...)
 end
 
