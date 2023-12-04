a4e177934c683b708d98ccac8b181cf5edf4e509
Torkel <torkel.loman@gmail.com>
2023-12-04 10:56:21 -0500

    init

---
+++ src/steady_state_stability.jl
@@ -0,0 +1,100 @@
+### Stability Analysis ###
+
+"""
+    stability(u::Vector{T}, rs::ReactionSystem, p; sparse=false, ss_jac = steady_state_jac(u, rs, p; sparse=sparse), t=Inf, non_autonomous_war=true)
+
+Compute the stability of a steady state (Returned as a `Bool`, with `true` indicating stability).
+
+Arguments:
+    - `u`: The steady state for which we want to compute stability.
+    - `rs`: The reaction system model for which we want to compute stability.
+    - `p`: The parameter set for which we want to compute stability.
+    - `sparse=false`: If we wish to create a sparse Jacobian for the stability computation.
+    - `ss_jac = steady_state_jac(u, rs; sparse=sparse)`: It is possible to pre-compute the Jacobian used for stability computation using `steady_state_jac`. If stability is computed for many states, precomputing the Jacobian may speed up evaluation.
+    - `t=Inf`: The time point at which stability is computed. 
+    - `non_autonomous_war=true`: If the system is non-autonomous (e.g. a rate depends on t), a warning will be given. Set this to false to remove that. Alternatively, specify a nonInf value for `t`.
+
+Example:
+```julia
+# Creates model.
+rn = @reaction_network begin
+    (p,d), 0 <--> X
+end
+p = [:p => 1.0, :d => 0.5]
+
+# Finds (the trivial) steady state, and computes stability.
+steady_state = [2.0]
+steady_state_stability(steady_state, rn, p)
+```
+
+Notes:
+    y states (each being a `Vector`) is provided as `u`, stability is computed for each state separately.
+"""
+function steady_state_stability(u::Vector{T}, rs::ReactionSystem, p; 
+                                sparse=false, ss_jac = steady_state_jac(rs; u0=u, sparse=sparse), t=Inf, non_autonomous_war=true) where T
+    # Warning checks.
+    !is_autonomous(rs) && non_autonomous_war && @warn "Attempting to compute stability for a non-autonomous system. Set `non_autonomous_war=false` to disable this warning."
+
+    # Because Jacobian currently requires ps to be a normal vector, can be removed once this get fixed in MTK.
+    if (u isa Vector{<:Pair}) || (u isa Dict) 
+        u_dict = Dict(symmap_to_varmap(rs, u))
+        u = [u_dict[var] for var in states(rs)]        
+    end
+    if (p isa Vector{<:Pair}) || (p isa Dict)
+        p_dict = Dict(symmap_to_varmap(rs, p))
+        p = [p_dict[var] for var in parameters(rs)]
+    end
+
+    # Computes stability
+    jac = ss_jac(u, p, Inf)
+    return maximum(real.(eigvals(jac))) < 0
+end
+# Computes the stability for a vector of steady states.
+function steady_state_stability(us::Vector{Vector{T}}, rs::ReactionSystem, p; sparse=false, ss_jac = steady_state_jac(rs; u0=us[1], sparse=sparse)) where T
+    return [steady_state_stability(u, rs, p) for u in us]
+end
+
+"""
+    steady_state_jac(rs::ReactionSystem; u0=[], sparse=false)
+
+Creates the Jacobian function which can be used as input to `steady_state_stability`. Useful when a large number of stability computation has to be carried out in a performant manner.
+ 
+Example:
+```julia
+# Creates model.
+rn = @reaction_network begin
+    (p,d), 0 <--> X
+end
+p = [:p => 1.0, :d => 0.5]
+
+# Creates the steady state jacobian.
+ss_jac = steady_state_jacobian(rn)
+
+# Finds (the trivial) steady state, and computes stability.
+steady_state = [2.0]
+steady_state_stability(steady_state, rn, p; ss_jac)
+```
+
+Arguments:
+    - `rs`: The reaction system model for which we want to compute stability.
+    - `u0=[]`: For systems with conservation laws, a `u` is required to compute the conserved quantities.
+    - `sparse=false`: If we wish to create a sparse Jacobian for the stability computation.
+"""
+function steady_state_jac(rs::ReactionSystem; u0=[], sparse=false) where T
+    # If u0 is a vector of values, must be converted to something MTK understands.
+    if (u0 isa Vector{<:Number})
+        if length(u0) == length(states(rs))
+            u0 = Pair.(states(rs), u0)
+        elseif !isempty(u0)
+            error("You are trying to compute a stability matrix, providing u0 to compute conservation laws. Your provided u0 vector has length < the number of system states. If you provide a u0 vector, these have to be identical.")
+        end
+    end
+
+    # Converts u0 to values MTK understands, and checks that potential conservation laws are accounted for.
+    u0 = symmap_to_varmap(rs, u0)
+    conservationlaw_errorcheck(rs, u0)
+
+    # Creates a ODESystem and jacobian.
+    osys = convert(ODESystem, rs; remove_conserved = true, defaults=Dict(u0))
+    return ODEFunction(osys; jac=true, sparse=sparse).jac
+end
---
+++ test/miscellaneous_tests/stability_computation.jl
@@ -0,0 +1,79 @@
+### Fetch Packages ###
+
+# Fetch packages.
+using Catalyst, OrdinaryDiffEq
+using Random, Test
+
+# Sets rnd number.
+using StableRNGs
+rng = StableRNG(12345)
+
+### Run Tests ###
+
+# Tests that stability is correctly assessed (using simulation) in multi stable system.
+# Tests that `steady_state_jac` function works.
+# Tests with and without sparsity.
+# tests using symbolic input.
+let
+    # System which may have between 1 and 7 fixed points.
+    rn = @reaction_network begin
+        v/20.0 + hillar(X,Y,v,K,n), 0 --> X
+        v/20.0 + hillar(Y,X,v,K,n), 0 --> Y
+        d, (X,Y) --> 0
+    end
+    ss_jac = steady_state_jac(rn)
+    ss_jac_sparse = steady_state_jac(rn; sparse=true)
+
+    # Repeats several times, most cases should be encountered several times.
+    for i = 1:50
+        # Generates random parameter values (which can generate all steady states cases).
+        p = [:v => 1.0 + 3*rand(rng), :K => 0.5 + 2*rand(rng), :n => rand(rng,[1,2,3,4]), :d => 0.5 + rand(rng)]
+
+        sss = hc_steady_states(rn, p)
+        stabs_1 = steady_state_stability(sss, rn, p)
+        stabs_2 = steady_state_stability(sss, rn, p; sparse=true)
+        stabs_3 = steady_state_stability(sss, rn, p; ss_jac=ss_jac)
+        stabs_4 = steady_state_stability(sss, rn, p; ss_jac=ss_jac_sparse)
+
+        for (idx,ss) in enumerate(sss)
+            oprob = ODEProblem(rn, [1.001, 0.999] .* ss, (0.0,1000.0), p)
+            sol_end = solve(oprob, Rosenbrock23())[end]
+            stabs_5 = ss ≈ sol_end
+            @test stabs_1[idx] == stabs_2[idx] == stabs_3[idx] == stabs_4[idx] == stabs_5
+        end
+    end
+end
+
+# Checks stability for system with known stability structure.
+# Tests for system with conservation laws.
+# Tests for various input forms of u0 and ps.
+let
+    rn = complete(@reaction_network begin
+        k1+Z, Y --> 2X
+        k2, 2X --> X + Y
+        k3, X + Y --> Y
+        k4, X --> 0
+        (kD1+X, kD2), 2Z <--> Z2
+    end)
+    @unpack k1, k2, k3, k4, kD1, kD2, X, Y, Z, Z2 = rn
+    u0_1 = [X => 1.0, Y => 1.0, Z => 1.0, Z2 => 1.0]
+    u0_2 = [:X => 1.0, :Y => 1.0, :Z => 1.0, :Z2 => 1.0]
+    u0_3 = [rn.X => 1.0, rn.Y => 1.0, rn.Z => 1.0, rn.Z2 => 1.0]
+    u0_4 = [1.0, 1.0, 1.0, 1.0]
+    ps_1 = [k1 => 8.0, k2 => 2.0, k3 => 1.0, k4 => 1.5, kD1 => 0.5, kD2 => 2.0]
+    ps_2 = [:k1 => 8.0, :k2 => 2.0, :k3 => 1.0, :k4 => 1.5, :kD1 => 0.5, :kD2 => 2.0]
+    ps_3 = [rn.k1 => 8.0, rn.k2 => 2.0, rn.k3 => 1.0, rn.k4 => 1.5, rn.kD1 => 0.5, rn.kD2 => 4.0]
+    ps_4 = [8.0, 2.0, 1.0, 1.5, 0.5, 4.0]
+    
+    sss = hc_steady_states(rn, ps_1; u0=u0_1)
+    for u0 in [u0_1, u0_2, u0_3, u0_4], ps in [ps_1, ps_2, ps_3, ps_4]
+        stab_1 = steady_state_stability(sss, rn, ps)
+        @test length(stab_1) == 3
+        @test count(stab_1) == 2
+    
+        ss_jac = steady_state_jac(rn; u0=u0)
+        stab_2 = steady_state_stability(sss, rn, ps; ss_jac=ss_jac)
+        @test length(stab_2) == 3
+        @test count(stab_2) == 2
+    end
+end
