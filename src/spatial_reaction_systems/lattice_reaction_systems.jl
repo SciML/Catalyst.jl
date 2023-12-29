b9ec8d82fc985f61f7da973a87adaabb65c87280
Torkel <torkel.loman@gmail.com>
2023-12-29 15:17:15 +0100

    init

--- src/spatial_reaction_systems/lattice_reaction_systems.jl
+++ src/spatial_reaction_systems/lattice_reaction_systems.jl
@@ -1,14 +1,14 @@
 ### Lattice Reaction Network Structure ###
 # Describes a spatial reaction network over a graph.
 # Adding the "<: MT.AbstractTimeDependentSystem" part messes up show, disabling me from creating LRSs.
-struct LatticeReactionSystem{S,T} # <: MT.AbstractTimeDependentSystem 
+struct LatticeReactionSystem{Q,R,S,T} # <: MT.AbstractTimeDependentSystem 
     # Input values.
     """The reaction system within each compartment."""
-    rs::ReactionSystem{S}
+    rs::ReactionSystem{Q}
     """The spatial reactions defined between individual nodes."""
-    spatial_reactions::Vector{T}
+    spatial_reactions::Vector{R}
     """The graph on which the lattice is defined."""
-    lattice::SimpleDiGraph{Int64}
+    lattice::S
 
     # Derived values.
     """The number of compartments."""
@@ -17,8 +17,7 @@ struct LatticeReactionSystem{S,T} # <: MT.AbstractTimeDependentSystem
     num_edges::Int64
     """The number of species."""
     num_species::Int64
-    """Whenever the initial input was a digraph."""
-    init_digraph::Bool
+
     """Species that may move spatially."""
     spat_species::Vector{BasicSymbolic{Real}}
     """
@@ -37,19 +36,29 @@ struct LatticeReactionSystem{S,T} # <: MT.AbstractTimeDependentSystem
     """
     edge_parameters::Vector{BasicSymbolic{Real}}
 
-    function LatticeReactionSystem(rs::ReactionSystem{S}, spatial_reactions::Vector{T},
-                                   lattice::DiGraph; init_digraph = true) where {S, T}
-        # There probably some better way to ascertain that T has that type. Not sure how.
-        if !(T <: AbstractSpatialReaction) 
+    """Whenever the initial input was a digraph."""
+    init_digraph::Bool
+    """
+    A list of all the edges on the lattice. 
+    The format depends on the type of lattice (Cartesian grid, grid, or graph).
+    """
+    edge_list::T
+
+    function LatticeReactionSystem(rs::ReactionSystem{Q}, spatial_reactions::Vector{R},
+                                   lattice::S, num_verts, edge_list::T; init_digraph = true) where {Q, R, S, T}
+        # Error checks.
+        if !(R <: AbstractSpatialReaction) |
             error("The second argument must be a vector of AbstractSpatialReaction subtypes.") 
         end
 
+        # Computes derived values spatial species. Counts the total number of species.
         if isempty(spatial_reactions)
             spat_species = Vector{BasicSymbolic{Real}}[]
         else
             spat_species = unique(reduce(vcat, [spatial_species(sr) for sr in spatial_reactions]))
         end
-        num_species = length(unique([species(rs); spat_species]))
+
+        # Computes the sets of vertex, edge, and all, parameters.
         rs_edge_parameters = filter(isedgeparameter, parameters(rs))
         if isempty(spatial_reactions)
             srs_edge_parameters = Vector{BasicSymbolic{Real}}[]
@@ -58,16 +67,46 @@ struct LatticeReactionSystem{S,T} # <: MT.AbstractTimeDependentSystem
         end
         edge_parameters = unique([rs_edge_parameters; srs_edge_parameters])
         vertex_parameters = filter(!isedgeparameter, parameters(rs))
+
+        # Counts the number of edges and species types (number of vertexes already counted).
+        num_edges = false
+        num_species = length(unique([species(rs); spat_species]))
+
         # Ensures the parameter order begins similarly to in the non-spatial ReactionSystem.
         ps = [parameters(rs); setdiff([edge_parameters; vertex_parameters], parameters(rs))]    
 
+        # Checks that all spatial reactions are valid for this reactions system.
         foreach(sr -> check_spatial_reaction_validity(rs, sr; edge_parameters=edge_parameters), spatial_reactions)   
-        return new{S,T}(rs, spatial_reactions, lattice, nv(lattice), ne(lattice), num_species, 
-                            init_digraph, spat_species, ps, vertex_parameters, edge_parameters)
+
+        return new{Q,R,S,T}(rs, spatial_reactions, lattice, num_verts, ne(lattice), num_species, 
+                            spat_species, ps, vertex_parameters, edge_parameters, init_digraph, edge_list)
     end
 end
-function LatticeReactionSystem(rs, srs, lat::SimpleGraph)
-    return LatticeReactionSystem(rs, srs, DiGraph(lat); init_digraph = false)
+
+# Creates a LatticeReactionSystem from a CartesianGrid lattice.
+function LatticeReactionSystem(rs, srs, lattice::CartesianGridRej{S,T}; diagonal_connections=false) where {S,T}
+    num_verts = prod(lattice.dims)
+    edge_list = false
+    return LatticeReactionSystem(rs, srs, lattice, num_verts, edge_list; init_digraph = false, edge_list)
+end
+
+# Creates a LatticeReactionSystem from a Boolean Array lattice.
+function LatticeReactionSystem(rs, srs, lattice::Array{Bool, T}; diagonal_connections=false) where {T}
+    num_verts = count(lattice)
+    edge_list = false
+    return LatticeReactionSystem(rs, srs, lattice, num_verts, edge_list; init_digraph = false, edge_list)
+end
+
+# Creates a LatticeReactionSystem from a DiGraph lattice.
+function LatticeReactionSystem(rs, srs, lattice::SimpleGraph)
+    num_verts = nv(lattice)
+    edge_list = false
+    return LatticeReactionSystem(rs, srs, lattice, num_verts, edge_list; init_digraph = false, edge_list)
+end
+
+# Creates a LatticeReactionSystem from a Graph lattice.
+function LatticeReactionSystem(rs, srs, lattice::SimpleGraph)
+    return LatticeReactionSystem(rs, srs, DiGraph(lattice); init_digraph = false)
 end
 
 ### Lattice ReactionSystem Getters ###
@@ -89,3 +128,10 @@ ModelingToolkit.nameof(lrs::LatticeReactionSystem) = nameof(lrs.rs)
 
 # Checks if a lattice reaction system is a pure (linear) transport reaction system.
 is_transport_system(lrs::LatticeReactionSystem) = all(sr -> sr isa TransportReaction, lrs.spatial_reactions)
+
+# Checks if a LatticeReactionSystem have a Cartesian grid lattice.
+has_cartesian_lattice(lrs::LatticeReactionsSystem) = lrs.lattice isa CartesianGridRej{S,T} where {S,T}
+# Checks if a LatticeReactionSystem have a regular grid lattice.
+has_cartesian_lattice(lrs::LatticeReactionsSystem) = lrs.lattice isa Array{Bool, T} where T
+# Checks if a LatticeReactionSystem have a graph lattice.
+has_cartesian_lattice(lrs::LatticeReactionsSystem) = lrs.lattice isa SimpleDiGraph
---
+++ test/spatial_reaction_systems/lattice_reaction_systems_lattice_types.jl
@@ -0,0 +1,8 @@
+### Preparations ###
+
+# Fetch packages.
+using Catalyst, Graphs, OrdinaryDiffEq, Test
+
+### Run Tests ###
+
+# Checks that some grids, created using different approaches, generates the same spatial structures.
