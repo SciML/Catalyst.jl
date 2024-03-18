81b10623cb06e917e23fb7f00cb20fc04391327a
Torkel <torkel.loman@gmail.com>
2024-03-18 19:06:43 -0400

    up

--- src/Catalyst.jl
+++ src/Catalyst.jl
@@ -33,7 +33,7 @@ import ModelingToolkit: get_variables, namespace_expr, namespace_equation, get_v
 # internal but needed ModelingToolkit functions
 import ModelingToolkit: check_variables,
                         check_parameters, _iszero, _merge, check_units,
-                        get_unit, check_equations
+                        get_unit, check_equations, iscomplete
 
 import Base: (==), hash, size, getindex, setindex, isless, Sort.defalg, length, show
 import MacroTools, Graphs
