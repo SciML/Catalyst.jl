ca6558a169cabc94ed17faaeb6864a97fe5acd31
Torkel Loman <torkel.loman@gmail.com>
2024-04-03 14:17:59 -0400

    Update test/miscellaneous_tests/symbolic_stoichiometry.jl
    Co-authored-by: Sam Isaacson <isaacsas@users.noreply.github.com>

--- test/miscellaneous_tests/symbolic_stoichiometry.jl
+++ test/miscellaneous_tests/symbolic_stoichiometry.jl
@@ -54,7 +54,7 @@ begin
     u0_1 = [A => 3.0, B => 2.0, C => 3.0, D => 5.0]
     ps_1 = [k => 5.0, α => 2.0]
     u0_2 = [Int64(u[2]) for u in u0_1]
-    ps_2 = [Int64(p[2]) for p in ps_1]
+    ps_2 = Tuple(p[2] for p in ps_1)
     τ = 1.5
 
     # Creates `ReactionSystem` model.
