3084fe7e3bf131200136af85e7d2406c64e1989f
Torkel <torkel.loman@gmail.com>
2024-04-03 16:43:11 -0400

    up

--- test/miscellaneous_tests/symbolic_stoichiometry.jl
+++ test/miscellaneous_tests/symbolic_stoichiometry.jl
@@ -56,7 +56,7 @@ begin
     @species A(t), B(t), C(t), D(t)
     u0_1 = (A => 3.0, B => 2.0, C => 3.0, D => 5.0)
     ps_1 = (k => 5.0, α => 2)
-    u0_2 = [Int64(u[2]) for u in u0_1]
+    u0_2 = [u[2] for u in u0_1]
     ps_2 = [p[2] for p in ps_1]
     τ = 1.5
 
