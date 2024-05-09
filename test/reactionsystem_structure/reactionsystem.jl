bc93f41722f25c1e2094328f93e8c121afc43b15
Torkel <torkel.loman@gmail.com>
2024-05-09 09:55:20 -0400

    up

--- test/reactionsystem_structure/reactionsystem.jl
+++ test/reactionsystem_structure/reactionsystem.jl
@@ -368,8 +368,9 @@ let
     obs = [Equation(L, 2 * x + y)]
     @named rs3 = ReactionSystem(rxs, t; observed = obs)
     L2 = L
-    @unpack L = rs3
-    @test_broken false #@test isequal(L, L2) # `@unpack` does not seem to work on observables.
+    @test_broken false # `@unpack` does not seem to work on observables. 
+    #@unpack L = rs3
+    #@test isequal(L, L2)
 end
 
 # Test that non-integer stoichiometry goes through.
