9298fb28d7f09203b6abd96b26a4520abb6994a5
Torkel <torkel.loman@gmail.com>
2024-05-14 19:02:41 -0400

    up

--- docs/pages.jl
+++ docs/pages.jl
@@ -5,30 +5,28 @@ pages = Any[
         "01_introduction_to_catalyst/02_introduction_to_catalyst.md"
         # Advanced introduction.
     ],
-    "Model Creation" => Any[
+    "Model Creation and Properties" => Any[
         "02_model_creation/01_dsl_description.md",
         # DSL - Advanced
         "02_model_creation/03_programmatic_CRN_construction.md",
         "02_model_creation/04_constraint_equations.md",
+        "02_model_creation/05_compositional_modeling.md",
         # Events.
-        "02_model_creation/06_compositional_modeling.md",
         # Distributed parameters, rates, and initial conditions.
         # Loading and writing models to files.
+        # Model visualisation.
+        "02_reaction_network_functionality/10_network_analysis.md",
+        "02_reaction_network_functionality/11_chemistry_related_functionality.md",
         "Model creation examples" => Any[
             "02_model_creation/examples/basic_CRN_examples.md",
             "02_model_creation/examples/hodgkin_huxley_equation.md",
             "02_model_creation/examples/smoluchowski_coagulation_equation.md"
         ]
     ],
-    "Reaction network-related functionalities" => Any[
-        # Model visualisation.
-        "03_reaction_network_functionality/network_analysis.md",
-        "03_reaction_network_functionality/chemistry_related_functionality.md",
-    ],
     "Model simulation" => Any[
         # Simulation introduction.
         # Simulation plotting.
-        "04_model_simulation/03_simulation_structure_interfacing.md",
+        "03_model_simulation/03_simulation_structure_interfacing.md",
         # Monte Carlo/Ensemble simulations.
         # Stochastic simulation statistical analysis.
         # ODE Performance considerations/advice.
@@ -36,31 +34,31 @@ pages = Any[
         # Jump Performance considerations/advice.
         # Finite state projection
     ],
-    "Spatial modelling" => Any[
-        # Intro.
-        # Lattice ODEs.
-        # Lattice Jumps.
-    ],
     "Steady state analysis" => Any[
-        "06_steady_state_functionality/01_homotopy_continuation.md",
-        "06_steady_state_functionality/02_nonlinear_solve.md",
+        "04_steady_state_functionality/01_homotopy_continuation.md",
+        "04_steady_state_functionality/02_nonlinear_solve.md",
         # Stability analysis.
-        "06_steady_state_functionality/04_bifurcation_diagrams.md"
+        "04_steady_state_functionality/04_bifurcation_diagrams.md"
         # Dynamical systems analysis.
     ],
     "Inverse Problems" => Any[
         # Inverse problems introduction.
-        "07_inverse_problems/02_optimization_ode_param_fitting.md",
+        "05_inverse_problems/02_optimization_ode_param_fitting.md",
         # "inverse_problems/03_petab_ode_param_fitting.md",
         # ODE parameter fitting using Turing.
         # SDE/Jump fitting.
         # Non-parameter fitting optimisation.
-        "07_inverse_problems/07_structural_identifiability.md",
+        "05_inverse_problems/07_structural_identifiability.md",
         # Practical identifiability.
         # GLobal and local sensitivity analysis.
         "Inverse problem examples" => Any[
-            "07_inverse_problems/examples/ode_fitting_oscillation.md"
+            "05_inverse_problems/examples/ode_fitting_oscillation.md"
         ]
+    ],
+    "Spatial modelling" => Any[
+        # Intro.
+        # Lattice ODEs.
+        # Lattice Jumps.
     ],
     # "Developer Documentation" => Any[
     #     # Contributor's guide.
