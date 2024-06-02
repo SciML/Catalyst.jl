6f62d14fb740aee2d7bf8aa2375d779a0be9b7ad
Torkel <torkel.loman@gmail.com>
2024-06-02 10:53:22 -0400

    init

--- docs/make.jl
+++ docs/make.jl
@@ -32,7 +32,7 @@ include("pages.jl")
 
 makedocs(sitename = "Catalyst.jl",
          authors = "Samuel Isaacson",
-         format = Documenter.HTML(; analytics = "UA-90474609-3",
+         format = Documenter.HTML(; analytics = "UA-90474609-3", collapselevel = 1,
                                   prettyurls = (get(ENV, "CI", nothing) == "true"),
                                   assets = ["assets/favicon.ico"],
                                   canonical = "https://docs.sciml.ai/Catalyst/stable/"),
