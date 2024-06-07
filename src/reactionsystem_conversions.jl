3106d1dfd253b15ced8d130232e08f65baa27345
Torkel Loman <torkel.loman@gmail.com>
2024-06-07 16:29:55 -0400

    Update src/reactionsystem_conversions.jl
    Co-authored-by: Sam Isaacson <isaacsas@users.noreply.github.com>

--- src/reactionsystem_conversions.jl
+++ src/reactionsystem_conversions.jl
@@ -596,8 +596,8 @@ Notes:
 function Base.convert(::Type{<:SDESystem}, rs::ReactionSystem;
         name = nameof(rs), combinatoric_ratelaws = get_combinatoric_ratelaws(rs),
         include_zero_odes = true, checks = false, remove_conserved = false,
-        default_u0 = Dict(), default_p = Dict(), defaults = _merge(
-            Dict(default_u0), Dict(default_p)),
+        default_u0 = Dict(), default_p = Dict(), 
+        defaults = _merge(Dict(default_u0), Dict(default_p)),
         kwargs...)
     iscomplete(rs) || error(COMPLETENESS_ERROR)
     spatial_convert_err(rs::ReactionSystem, SDESystem)
