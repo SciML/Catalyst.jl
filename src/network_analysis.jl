64d83fda8c93373764239d965c8d743a8817bd13
vyudu <vincent.duyuan@gmail.com>
2024-06-18 15:55:09 -0400

    rebase

--- src/network_analysis.jl
+++ src/network_analysis.jl
@@ -359,51 +359,6 @@ end
 
 linkageclasses(incidencegraph) = Graphs.connected_components(incidencegraph)
 
-"""
-    stronglinkageclasses(rn::ReactionSystem)
-
-    Return the strongly connected components of a reaction network's incidence graph (i.e. sub-groups of reaction complexes such that every complex is reachable from every other one in the sub-group).
-"""
-
-function stronglinkageclasses(rn::ReactionSystem) 
-    nps = get_networkproperties(rn)
-    if isempty(nps.stronglinkageclasses)
-        nps.stronglinkageclasses = stronglinkageclasses(incidencematgraph(rn))
-    end
-    nps.stronglinkageclasses
-end
-
-stronglinkageclasses(incidencegraph) = Graphs.strongly_connected_components(incidencegraph)
-
-"""
-    terminallinkageclasses(rn::ReactionSystem)
-
-    Return the terminal strongly connected components of a reaction network's incidence graph (i.e. sub-groups of reaction complexes that are 1) strongly connected and 2) every reaction in the component produces a complex in the component).
-"""
-
-function terminallinkageclasses(rn::ReactionSystem) 
-    nps = get_networkproperties(rn)
-    if isempty(nps.terminallinkageclasses)
-        slcs = stronglinkageclasses(rn)
-        tslcs = filter(lc->isterminal(lc, rn), slcs)
-        nps.terminallinkageclasses = tslcs
-    end
-    nps.terminallinkageclasses
-end
-
-function isterminal(lc::Vector, rn::ReactionSystem) 
-    imat = incidencemat(rn)
-
-    for r in 1:size(imat, 2)
-        s = findfirst(==(-1), @view imat[:, r])
-        if s in Set(lc)
-            p = findfirst(==(1), @view imat[:, r])
-            p in Set(lc) ? continue : return false
-        end
-    end
-    true
-end
-
 @doc raw"""
     deficiency(rn::ReactionSystem)
 
