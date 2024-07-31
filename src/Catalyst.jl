ce7bf37afd1ba488602293fe6abf60daacf1608a
Christopher Rackauckas <accounts@chrisrackauckas.com>
2024-07-31 08:54:28 -0400

    Update Catalyst.jl

--- src/Catalyst.jl
+++ src/Catalyst.jl
@@ -64,7 +64,9 @@ function __init__()
         USE_GV_JLL[] = true
         let cfg = joinpath(Graphviz_jll.artifact_dir, "lib", "graphviz", "config6")
             if !isfile(cfg)
-                Graphviz_jll.dot(path -> run(`$path -c`))
+                Graphviz_jll.dot() do path 
+                  run(`$path -c`)
+                end
             end
         end
     end
