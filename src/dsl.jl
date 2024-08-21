cc418bf06883b05f575a41ca050532fe53aa2df5
Sam Isaacson <isaacsas@users.noreply.github.com>
2024-08-21 13:55:06 -0400

    update kwargs

--- src/dsl.jl
+++ src/dsl.jl
@@ -388,18 +388,17 @@ function make_reaction_system(ex::Expr; name = :(gensym(:ReactionSystem)))
         $comps
         $diffexpr
 
-        sivs_vec = $sivs
+        spatial_ivs = $sivs
         rx_eq_vec = $rxexprs
         vars = setdiff(union($spssym, $varssym, $compssym), $obs_syms)
-        obseqs = $observed_eqs
-        cevents = $continuous_events_expr
-        devents = $discrete_events_expr
+        observed = $observed_eqs
+        continuous_events = $continuous_events_expr
+        discrete_events = $discrete_events_expr
 
         remake_ReactionSystem_internal(
             make_ReactionSystem_internal(
-                rx_eq_vec, $tiv, vars, $pssym;
-                name = $name, spatial_ivs = sivs_vec, observed = obseqs,
-                continuous_events = cevents, discrete_events = devents,
+                rx_eq_vec, $tiv, vars, $pssym; name = $name, spatial_ivs, observed,
+                continuous_events, discrete_events,
                 combinatoric_ratelaws = $combinatoric_ratelaws);
             default_reaction_metadata = $default_reaction_metadata)
     end
