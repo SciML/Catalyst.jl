c1cfacf71015adf146d63f6fdec07df4a0ffcdda
Sam Isaacson <isaacsas@users.noreply.github.com>
2024-11-01 10:22:09 -0400

    comments

--- src/reactionsystem.jl
+++ src/reactionsystem.jl
@@ -1402,6 +1402,11 @@ end
 
 Compose the indicated [`ReactionSystem`](@ref) with one or more `AbstractSystem`s.
 
+Notes:
+- The `AbstractSystem` being added in must be an `ODESystem`, `NonlinearSystem`,
+  or `ReactionSystem` currently.
+- Returns a new `ReactionSystem` and does not modify `rs`.
+- By default, the new `ReactionSystem` will have the same name as `sys`.
 """
 function ModelingToolkit.compose(sys::ReactionSystem, systems::AbstractArray; name = nameof(sys))
     nsys = length(systems)
