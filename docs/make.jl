2b4a70bc63b469952fcffee368d37d5edf38c4c6
Sam Isaacson <isaacsas@users.noreply.github.com>
2024-11-13 17:10:56 -0500

    only build pages in pages.jl

--- docs/make.jl
+++ docs/make.jl
@@ -41,7 +41,7 @@ makedocs(sitename = "Catalyst.jl",
     doctest = false,
     clean = true,
     pages = pages,
-    pagesonly = false,
+    pagesonly = true,
     warnonly = [:missing_docs])
 
 deploydocs(repo = "github.com/SciML/Catalyst.jl.git";
