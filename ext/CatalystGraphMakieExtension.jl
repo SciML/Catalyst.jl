69f69aeb22a732eee8650f03fbe2f401229dc85f
vyudu <vincent.duyuan@gmail.com>
2024-11-14 14:23:18 -0500

    graph init

--- ext/CatalystGraphMakieExtension.jl
+++ ext/CatalystGraphMakieExtension.jl
@@ -1,11 +1,13 @@
 module CatalystGraphMakieExtension
 
 # Fetch packages.
-using Catalyst, GraphMakie
+using Catalyst, GraphMakie, Graphs 
 import Catalyst: lattice_plot, lattice_animation, extract_vals
-import Graphs: AbstractGraph, SimpleGraph
+import Graphs: AbstractGraph, SimpleGraph, SimpleDiGraph, SimpleEdge, src, dst, ne, nv 
+import Catalyst: speciesreactiongraph, incidencematgraph
 
 # Creates and exports hc_steady_states function.
 include("CatalystGraphMakieExtension/graph_makie_extension_spatial_modelling.jl")
-
+include("CatalystGraphMakieExtension/rn_graph_plot.jl")
+export SRGraph, ComplexGraph
 end
