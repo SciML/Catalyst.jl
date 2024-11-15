8d095941ec85f833346077aa84f590086200be79
Sam Isaacson <isaacsas@users.noreply.github.com>
2024-11-15 15:02:18 -0500

    typo

--- src/reactionsystem_conversions.jl
+++ src/reactionsystem_conversions.jl
@@ -721,7 +721,7 @@ function Base.convert(::Type{<:JumpSystem}, rs::ReactionSystem; name = nameof(rs
         combinatoric_ratelaws = get_combinatoric_ratelaws(rs),
         remove_conserved = nothing, checks = false,
         default_u0 = Dict(), default_p = Dict(),
-        defaults = _merge(Dict(default_u0), Dict(default_p)), physical_scales = nothing
+        defaults = _merge(Dict(default_u0), Dict(default_p)), physical_scales = nothing,
         kwargs...)
     iscomplete(rs) || error(COMPLETENESS_ERROR)
     spatial_convert_err(rs::ReactionSystem, JumpSystem)
