794fb04418c4555e58170a583cb1508c130471da
Vincent Du <54586336+vyudu@users.noreply.github.com>
2024-11-16 10:39:08 -0500

    Update ext/CatalystGraphMakieExtension.jl
    Co-authored-by: Sam Isaacson <isaacsas@users.noreply.github.com>

--- ext/CatalystGraphMakieExtension.jl
+++ ext/CatalystGraphMakieExtension.jl
@@ -1,8 +1,8 @@
 module CatalystGraphMakieExtension
 
 # Fetch packages.
-using Catalyst, GraphMakie, Graphs 
-import Catalyst: lattice_plot, lattice_animation, extract_vals, get_variables!
+using Catalyst, GraphMakie, Graphs, Symbolics
+using Symbolics: get_variables!
 import Catalyst: species_reaction_graph, incidencematgraph
 
 # Creates and exports graph plotting functions.
