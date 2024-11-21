75999625498cbf4d278c2b72e9cb4829571453a6
vyudu <vincent.duyuan@gmail.com>
2024-11-21 14:23:05 -0500

    fix

--- test/runtests.jl
+++ test/runtests.jl
@@ -90,7 +90,7 @@ end
 
         # Test spatial plotting, using CarioMakie and GraphMakie
         @time @safetestset "Lattice Simulation Plotting" begin include("extensions/lattice_simulation_plotting.jl") end
-        @time @safetestset "Graph visualization" begin include("extensions/graphmakie.jl")
+        @time @safetestset "Graph visualization" begin include("extensions/graphmakie.jl") end
     end
 
 end # @time
