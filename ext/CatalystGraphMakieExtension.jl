d854bfd01c9ccb47efbca7dea7074f68ea07abab
vyudu <vincent.duyuan@gmail.com>
2024-11-25 16:16:15 -0500

    init

--- ext/CatalystGraphMakieExtension.jl
+++ ext/CatalystGraphMakieExtension.jl
@@ -1,9 +1,10 @@
 module CatalystGraphMakieExtension
 
 # Fetch packages.
-using Catalyst, GraphMakie, Graphs, Symbolics
+using Catalyst, GraphMakie, Graphs, Symbolics, SparseArrays
 using Symbolics: get_variables!
 import Catalyst: species_reaction_graph, incidencematgraph, lattice_plot, lattice_animation
+import SparseArrays: rowvals, nonzeros, nzrange
 
 # Creates and exports graph plotting functions.
 include("CatalystGraphMakieExtension/graph_makie_extension_spatial_modelling.jl")
