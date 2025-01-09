57c8e2dd3e326fae1a3bcec3f809fd2d7bd36276
Sam Isaacson <isaacsas@users.noreply.github.com>
2025-01-09 09:00:20 -0500

    change up extension conditional

--- ext/CatalystStructuralIdentifiabilityExtension.jl
+++ ext/CatalystStructuralIdentifiabilityExtension.jl
@@ -5,9 +5,9 @@ using Catalyst
 import DataStructures.OrderedDict
 import StructuralIdentifiability as SI
 
-# Creates and exports make_si_ode function.
-if VERSION >= v"1.11.2"
-    include("CatalystStructuralIdentifiabilityExtension/structural_identifiability_extension.jl")
-else
-    @warn "Due to https://github.com/SciML/StructuralIdentifiability.jl/issues/360, CatalystStructuralIdentifiabilityExtension.jl is only compatible with Julia v1.11.2 or later."
+@static if VERSION < v"1.11.2"
+    @warn "Due to https://github.com/SciML/StructuralIdentifiability.jl/issues/360, CatalystStructuralIdentifiabilityExtension.jl is only tested with Julia v1.11.2 or later. Its use is not recommended on earlier Julia versions."
 end
+
+# Creates and exports make_si_ode function.
+include("CatalystStructuralIdentifiabilityExtension/structural_identifiability_extension.jl")
