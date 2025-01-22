84ce6e8a235c206584524afea60c300942e78743
Torkel Loman <torkel.loman@gmail.com>
2025-01-22 10:24:00 +0000

    Update CatalystStructuralIdentifiabilityExtension.jl to 1.11.3 requirement

--- ext/CatalystStructuralIdentifiabilityExtension.jl
+++ ext/CatalystStructuralIdentifiabilityExtension.jl
@@ -5,7 +5,7 @@ using Catalyst
 import DataStructures.OrderedDict
 import StructuralIdentifiability as SI
 
-@static if VERSION < v"1.11.2"
+@static if VERSION < v"1.11.3"
     @warn "CatalystStructuralIdentifiabilityExtension.jl is only tested with Julia v1.11.2 or later due to https://github.com/SciML/StructuralIdentifiability.jl/issues/360. Its use is not recommended on earlier Julia versions."
 end
 
