00d9a419742d63e760657239a4829e444c5840db
vyudu <vincent.duyuan@gmail.com>
2025-02-04 20:14:35 -0500

    init multidocumenter

--- docs/make.jl
+++ docs/make.jl
@@ -2,6 +2,7 @@ using Documenter
 using Catalyst, ModelingToolkit
 # Add packages for plotting
 using GraphMakie, CairoMakie
+using MultiDocumenter
 
 docpath = Base.source_dir()
 assetpath = joinpath(docpath, "src", "assets")
@@ -48,5 +49,32 @@ makedocs(sitename = "Catalyst.jl",
     pagesonly = true,
     warnonly = [:missing_docs])
 
+clonedir = mktempdir()
+
+docs = [MultiDocumenter.MultiDocRef(
+        upstream = joinpath(@__DIR__, "build"),
+        path = "docs",
+        name = "Catalyst",
+        fix_canonical_url = false,
+        ),
+        MultiDocumenter.MultiDocRef(
+        upstream = joinpath(clonedir, "build"),
+        path = "NetworkAnalysis",
+        name = "CatalystNetworkAnalysis",
+        giturl = "https://github.com/SciML/CatalystNetworkAnalysis.jl",
+        )]
+
 deploydocs(repo = "github.com/SciML/Catalyst.jl.git";
     push_preview = true)
+
+outpath = joinpath(@__DIR__, "build")
+MultiDocumenter.make(outpath, docs;
+                     assets_dir = "docs/src/assets",
+                     search_engine = MultiDocumenter.SearchConfig(index_versions = [
+                                                                      "stable",
+                                                                  ],
+                                                                  engine = MultiDocumenter.FlexSearch),
+                     brand_image = MultiDocumenter.BrandImage("https://sciml.ai",
+                                                              joinpath("assets",
+                                                                       "logo.png")))
+
