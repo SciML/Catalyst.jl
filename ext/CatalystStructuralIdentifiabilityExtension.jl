83e209fd43238aa7e9b24310a6209e329c7479d4
Torkel Loman <torkel.loman@gmail.com>
2025-03-09 15:22:07 +0000

    Remove <v1.11 warning (seems to work across all versions)

--- ext/CatalystStructuralIdentifiabilityExtension.jl
+++ ext/CatalystStructuralIdentifiabilityExtension.jl
@@ -5,10 +5,6 @@ using Catalyst
 import DataStructures.OrderedDict
 import StructuralIdentifiability as SI
 
-@static if VERSION < v"1.11.3"
-    @warn "CatalystStructuralIdentifiabilityExtension.jl is only tested with Julia v1.11.2 or later due to https://github.com/SciML/StructuralIdentifiability.jl/issues/360. Its use is not recommended on earlier Julia versions."
-end
-
 # Creates and exports make_si_ode function.
 include("CatalystStructuralIdentifiabilityExtension/structural_identifiability_extension.jl")
 
