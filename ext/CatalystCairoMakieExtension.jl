b44afaeaf1ddc2eeafef56e9cbc134b027706cce
Christopher Rackauckas <accounts@chrisrackauckas.com>
2025-08-10 15:20:31 -0400

    Update ext/CatalystCairoMakieExtension.jl

--- ext/CatalystCairoMakieExtension.jl
+++ ext/CatalystCairoMakieExtension.jl
@@ -4,6 +4,7 @@ module CatalystCairoMakieExtension
 using Catalyst: Catalyst, lattice_plot, lattice_animation, lattice_kymograph,
                 demask_vals, extract_vals, extract_grid_axes
 using CairoMakie: CairoMakie
+import CairoMakie: lattice_plot
 using SparseArrays: SparseArrays
 
 # Creates and exports utilities for plotting lattice simulations.
